736c71ba3288ffb44b52ac59b1b5e3443dc48500
David Howells <dhowells@redhat.com>
2013-01-15 15:33:42 +0000

    PEFILE: Load the contained key if we consider the container to be validly signed
    Load the key contained in the PE binary if the signature on the container can
    be verified by following the chain of X.509 certificates in the PKCS#7 message
    to a key that we already trust.  Typically, the trusted key will be acquired
    from a source outside of the kernel, such as the UEFI database.
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- crypto/asymmetric_keys/pefile_parser.c
+++ crypto/asymmetric_keys/pefile_parser.c
@@ -395,6 +395,8 @@ static int pefile_key_preparse(struct key_preparsed_payload *prep)
 {
 	struct pkcs7_message *pkcs7;
 	struct pefile_context ctx;
+	const void *saved_data;
+	size_t saved_datalen;
 	int ret;
 
 	kenter("");
@@ -440,7 +442,14 @@ static int pefile_key_preparse(struct key_preparsed_payload *prep)
 	if (ret < 0)
 		goto error;
 
-	ret = -ENOANO; // Not yet complete
+	/* We can now try to load the key */
+	saved_data = prep->data;
+	saved_datalen = prep->datalen;
+	prep->data += ctx.keylist_offset;
+	prep->datalen = ctx.keylist_len;
+	ret = x509_key_preparse(prep);
+	prep->data = saved_data;
+	prep->datalen = saved_datalen;
 
 error:
 	pkcs7_free_message(ctx.pkcs7);
