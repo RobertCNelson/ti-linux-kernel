88e108cfc9eff000b57fc8e0b2991880e21635ee
Andrew Morton <akpm@linux-foundation.org>
2013-02-07 12:26:45 +1100

    mm-memory-failurec-fix-wrong-num_poisoned_pages-in-handling-memory-error-on-thp-fix
    tweak comment
    
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -1040,11 +1040,11 @@ int memory_failure(unsigned long pfn, int trapno, int flags)
 	}
 
 	/*
-	 * Currently errors on hugetlbfs pages are contained in hugepage
-	 * unit, so nr_pages should be 1 << compound_order. OTOH when
-	 * errors are on transparent hugepages, they are supposed to be
-	 * split and error containment is done in normal page unit.
-	 * So nr_pages should be one in this case.
+	 * Currently errors on hugetlbfs pages are measured in hugepage units,
+	 * so nr_pages should be 1 << compound_order.  OTOH when errors are on
+	 * transparent hugepages, they are supposed to be split and error
+	 * measurement is done in normal page units.  So nr_pages should be one
+	 * in this case.
 	 */
 	if (PageHuge(p))
 		nr_pages = 1 << compound_order(hpage);
