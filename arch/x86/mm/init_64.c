3efb2e7d3d5b983d3c5e59bff36621304490e125
Lin Feng <linfeng@cn.fujitsu.com>
2013-02-07 12:26:25 +1100

    memory-hotplug: introduce CONFIG_HAVE_BOOTMEM_INFO_NODE and revert register_page_bootmem_info_node() when platform not support
    It's implemented by adding a new Kconfig option named
    CONFIG_HAVE_BOOTMEM_INFO_NODE, which will be automatically selected by
    memory-hotplug feature fully supported archs(currently only on x86_64).
    
    Signed-off-by: Lin Feng <linfeng@cn.fujitsu.com>
    Reported-by: Michal Hocko <mhocko@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -1034,7 +1034,7 @@ vmemmap_populate(struct page *start_page, unsigned long size, int node)
 	return 0;
 }
 
-#ifdef CONFIG_MEMORY_HOTPLUG_SPARSE
+#if defined(CONFIG_MEMORY_HOTPLUG_SPARSE) && defined(CONFIG_HAVE_BOOTMEM_INFO_NODE)
 void register_page_bootmem_memmap(unsigned long section_nr,
 				  struct page *start_page, unsigned long size)
 {
