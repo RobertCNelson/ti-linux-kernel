5577b4a1f2b32152e1a77657cfc0cf842808fdfd
Glauber Costa <glommer@parallels.com>
2013-02-07 12:26:58 +1100

    memcg: avoid dangling reference count in creation failure.
    When use_hierarchy is enabled, we acquire an extra reference count in our
    parent during cgroup creation.  We don't release it, though, if any
    failure exist in the creation process.
    
    Signed-off-by: Glauber Costa <glommer@parallels.com>
    Reported-by: Michal Hocko <mhocko@suse.cz>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Hiroyuki Kamezawa <kamezawa.hiroyuki@gmail.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6197,6 +6197,8 @@ mem_cgroup_css_online(struct cgroup *cont)
 		 * call __mem_cgroup_free, so return directly
 		 */
 		mem_cgroup_put(memcg);
+		if (parent->use_hierarchy)
+			mem_cgroup_put(parent);
 	}
 	return error;
 }
