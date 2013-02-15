6f3d5a8f38eb23f792b408d9bc8fee53a7d158f9
Ingo Molnar <mingo@kernel.org>
2013-02-15 12:43:22 +0100

    Revert "mm: use aligned zone start for pfn_to_bitidx calculation"
    This reverts commit c060f943d0929f3e429c5d9522290584f6281d6e.
    
    Causes lockups.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5590,7 +5590,7 @@ static inline int pfn_to_bitidx(struct zone *zone, unsigned long pfn)
 	pfn &= (PAGES_PER_SECTION-1);
 	return (pfn >> pageblock_order) * NR_PAGEBLOCK_BITS;
 #else
-	pfn = pfn - round_down(zone->zone_start_pfn, pageblock_nr_pages);
+	pfn = pfn - zone->zone_start_pfn;
 	return (pfn >> pageblock_order) * NR_PAGEBLOCK_BITS;
 #endif /* CONFIG_SPARSEMEM */
 }
