7dbc68fcb0cdce27737dba9ee252f26d39d75fb6
Al Viro <viro@zeniv.linux.org.uk>
2013-02-14 22:38:02 -0500

    shmem_setup_file(): use d_alloc_pseudo() instead of d_alloc()
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2876,7 +2876,7 @@ struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags
 	struct file *res;
 	struct inode *inode;
 	struct path path;
-	struct dentry *root;
+	struct super_block *sb;
 	struct qstr this;
 
 	if (IS_ERR(shm_mnt))
@@ -2892,14 +2892,14 @@ struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags
 	this.name = name;
 	this.len = strlen(name);
 	this.hash = 0; /* will go */
-	root = shm_mnt->mnt_root;
-	path.dentry = d_alloc(root, &this);
+	sb = shm_mnt->mnt_sb;
+	path.dentry = d_alloc_pseudo(sb, &this);
 	if (!path.dentry)
 		goto put_memory;
 	path.mnt = mntget(shm_mnt);
 
 	res = ERR_PTR(-ENOSPC);
-	inode = shmem_get_inode(root->d_sb, NULL, S_IFREG | S_IRWXUGO, 0, flags);
+	inode = shmem_get_inode(sb, NULL, S_IFREG | S_IRWXUGO, 0, flags);
 	if (!inode)
 		goto put_dentry;
 
