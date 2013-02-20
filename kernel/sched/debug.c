e5d3623e83693a45c52ab57c66401389fdc1f3dc
Andrew Morton <akpm@linux-foundation.org>
2013-02-20 13:13:51 +1100

    sched-proc-sched_debug-fails-on-very-very-large-machines-v2-fix
    fix spello in comment
    
    Cc: Dave Jones <davej@redhat.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Nathan Zimmer <nzimmer@sgi.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/sched/debug.c
+++ kernel/sched/debug.c
@@ -407,7 +407,7 @@ void sysrq_sched_debug_show(void)
 }
 
 /*
- * This itererator needs some explination.
+ * This itererator needs some explanation.
  * It returns 1 for the header position.
  * This means 2 is cpu 0.
  * In a hotplugged system some cpus, including cpu 0, may be missing so we have
