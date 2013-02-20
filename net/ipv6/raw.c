580291267dca52720ec729b54611c07532a0a813
Andrew Morton <akpm@linux-foundation.org>
2013-02-20 13:17:04 +1100

    hlist-drop-the-node-parameter-from-iterators-fix-fix-fix-fix-fix
    drop bogus hunk from net/ipv6/raw.c
    
    Cc: Sasha Levin <sasha.levin@oracle.com>
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- net/ipv6/raw.c
+++ net/ipv6/raw.c
@@ -97,24 +97,6 @@ static struct sock *__raw_v6_lookup(struct net *net, struct sock *sk,
 			}
 			goto found;
 		}
-		sk_for_each_from (sk)
-			if (inet_sk(sk)->inet_num == num) {
-				struct ipv6_pinfo *np=inet6_sk(sk);
-				if (!net_eq(sock_net(sk), net))
-					continue;
-					if (!ipv6_addr_any(&np->daddr) && !ipv6_addr_equal(&np->daddr, rmt_addr))
-						continue;
-						if (sk->sk_bound_dev_if && sk->sk_bound_dev_if != dif)
-							continue;
-							if (!ipv6_addr_any(&np->rcv_saddr)) {
-								if (ipv6_addr_equal(&np->rcv_saddr, loc_addr))
-									goto found;
-									if (is_multicast && inet6_mc_check(sk, loc_addr, rmt_addr))
-										goto found;
-										continue;
-									}
-									goto found;
-								}
 	sk = NULL;
 found:
 	return sk;
