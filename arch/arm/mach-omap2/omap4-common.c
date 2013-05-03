e3503594380fd91284c99b100eccc7666e4fdbba
Rajendra Nayak <rnayak@ti.com>
2013-05-03 15:34:40 +0530

    ARM: DRA7: PM: Avoid all SAR save on dra7
    Get rid of all assumptions about always having a sar base on *all*
    omap4+ platforms. We dont need one on DRA7.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>

--- arch/arm/mach-omap2/omap4-common.c
+++ arch/arm/mach-omap2/omap4-common.c
@@ -239,7 +239,10 @@ omap_early_initcall(omap_l2_cache_init);
 
 void __iomem *omap4_get_sar_ram_base(void)
 {
-	return sar_ram_base;
+	if (sar_ram_base)
+		return sar_ram_base;
+	else
+		return NULL;
 }
 
 /*
