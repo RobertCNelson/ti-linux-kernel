254eb7a5f129a38448720db7727aa8b0f94ec729
Afzal Mohammed <afzal@ti.com>
2013-06-17 18:38:00 +0530

    ARM: OMAP2+: AM43x: determine features
    Determine AM43x device features by reusing AM335x helper as feature
    register layout is similar.
    
    Signed-off-by: Afzal Mohammed <afzal@ti.com>

--- arch/arm/mach-omap2/io.c
+++ arch/arm/mach-omap2/io.c
@@ -608,6 +608,7 @@ void __init am43xx_init_early(void)
 	omap_prm_base_init();
 	omap_cm_base_init();
 	omap3xxx_check_revision();
+	am33xx_check_features();
 	am43xx_powerdomains_init();
 	am43xx_clockdomains_init();
 	am33xx_hwmod_init();
