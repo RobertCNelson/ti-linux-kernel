e7f9208fe7e732d64b124dceb522d56f4d0f13b0
Sekhar Nori <nsekhar@ti.com>
2013-10-05 06:46:50 +0530

    ARM: OMAP2+: AM43xx: implement support for machine restart
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>

--- arch/arm/mach-omap2/am33xx-restart.c
+++ arch/arm/mach-omap2/am33xx-restart.c
@@ -9,6 +9,7 @@
 #include <linux/reboot.h>
 
 #include "common.h"
+#include "prcm43xx.h"
 #include "prm-regbits-33xx.h"
 #include "prm33xx.h"
 
@@ -31,5 +32,27 @@ void am33xx_restart(enum reboot_mode mode, const char *cmd)
 
 	/* OCP barrier */
 	(void)am33xx_prm_read_reg(AM33XX_PRM_DEVICE_MOD,
+				  AM33XX_PRM_RSTCTRL_OFFSET);
+}
+
+/**
+ * am43xx_restart - trigger a software restart of the SoC
+ * @mode: the "reboot mode", see arch/arm/kernel/{setup,process}.c
+ * @cmd: passed fr-om the userspace program rebooting the system (if provided)
+ *
+ * Resets the SoC.  For @cmd, see the 'reboot' syscall in
+ * kernel/sys.c.  No return value.
+ */
+void am43xx_restart(enum reboot_mode mode, const char *cmd)
+{
+	/* TODO: Handle mode and cmd if necessary */
+
+	am33xx_prm_rmw_reg_bits(AM33XX_RST_GLOBAL_WARM_SW_MASK,
+				AM33XX_RST_GLOBAL_WARM_SW_MASK,
+				AM43XX_PRM_DEVICE_INST,
+				AM33XX_PRM_RSTCTRL_OFFSET);
+
+	/* OCP barrier */
+	(void)am33xx_prm_read_reg(AM43XX_PRM_DEVICE_INST,
 				  AM33XX_PRM_RSTCTRL_OFFSET);
 }
