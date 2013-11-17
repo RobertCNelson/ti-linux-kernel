TI-OpenLink/wl18xx

wlcore: add testmode smart_config set group key command
Add testmode command to configure a group key for
a smart config group.

Signed-off-by: Eliad Peller <eliad@wizery.com>

drivers/net/wireless/ti/wlcore/testmode.c
@@ -390,6 +390,40 @@ out:
 	return ret;
 }
 
+static int wlcore_tm_cmd_smart_config_set_group_key(struct wl1271 *wl,
+						    struct nlattr *tb[])
+{
+	int ret;
+
+	wl1271_debug(DEBUG_CMD, "testmode cmd smart config set group key");
+
+	if (!tb[WL1271_TM_ATTR_GROUP_ID] ||
+	    !tb[WL1271_TM_ATTR_GROUP_KEY])
+		return -EINVAL;
+
+	mutex_lock(&wl->mutex);
+
+	if (unlikely(wl->state != WLCORE_STATE_ON)) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	ret = wl1271_ps_elp_wakeup(wl);
+	if (ret < 0)
+		goto out;
+
+	ret = wlcore_cmd_smart_config_set_group_key(wl,
+			nla_get_u32(tb[WL1271_TM_ATTR_GROUP_ID]),
+			nla_len(tb[WL1271_TM_ATTR_GROUP_KEY]),
+			nla_data(tb[WL1271_TM_ATTR_GROUP_KEY]));
+
+	wl1271_ps_elp_sleep(wl);
+out:
+	mutex_unlock(&wl->mutex);
+
+	return ret;
+}
+
 int wl1271_tm_cmd(struct ieee80211_hw *hw, void *data, int len)
 {
 	struct wl1271 *wl = hw->priv;
@@ -426,6 +460,8 @@ int wl1271_tm_cmd(struct ieee80211_hw *hw, void *data, int len)
 		return wlcore_tm_cmd_smart_config_start(wl, tb);
 	case WL1271_TM_CMD_SMART_CONFIG_STOP:
 		return wlcore_tm_cmd_smart_config_stop(wl, tb);
+	case WL1271_TM_CMD_SMART_CONFIG_SET_GROUP_KEY:
+		return wlcore_tm_cmd_smart_config_set_group_key(wl, tb);
 	default:
 		return -EOPNOTSUPP;
 	}

RobertCNelson/ti-linux-kernel

drivers/net/wireless/ti/wlcore/testmode.c
@@ -391,6 +391,40 @@ out:
 	return ret;
 }
 
+static int wlcore_tm_cmd_smart_config_set_group_key(struct wl1271 *wl,
+						    struct nlattr *tb[])
+{
+	int ret;
+
+	wl1271_debug(DEBUG_CMD, "testmode cmd smart config set group key");
+
+	if (!tb[WL1271_TM_ATTR_GROUP_ID] ||
+	    !tb[WL1271_TM_ATTR_GROUP_KEY])
+		return -EINVAL;
+
+	mutex_lock(&wl->mutex);
+
+	if (unlikely(wl->state != WLCORE_STATE_ON)) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	ret = wl1271_ps_elp_wakeup(wl);
+	if (ret < 0)
+		goto out;
+
+	ret = wlcore_smart_config_set_group_key(wl,
+			nla_get_u32(tb[WL1271_TM_ATTR_GROUP_ID]),
+			nla_len(tb[WL1271_TM_ATTR_GROUP_KEY]),
+			nla_data(tb[WL1271_TM_ATTR_GROUP_KEY]));
+
+	wl1271_ps_elp_sleep(wl);
+out:
+	mutex_unlock(&wl->mutex);
+
+	return ret;
+}
+
 int wl1271_tm_cmd(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 		  void *data, int len)
 {
@@ -428,6 +462,8 @@ int wl1271_tm_cmd(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 		return wlcore_tm_cmd_smart_config_start(wl, tb);
 	case WL1271_TM_CMD_SMART_CONFIG_STOP:
 		return wlcore_tm_cmd_smart_config_stop(wl, tb);
+	case WL1271_TM_CMD_SMART_CONFIG_SET_GROUP_KEY:
+		return wlcore_tm_cmd_smart_config_set_group_key(wl, tb);
 	default:
 		return -EOPNOTSUPP;
 	}