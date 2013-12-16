b2c5b05789389022d1544e29997502b50f4e9e76
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2013-12-16 18:16:44 +0100

    usb: musb: Revert "usb: musb: musb_core: Destroy dma controller only once"
    This reverts commit 90c17b09d8f3279980d5e9c100eef6c62fd843f3.
    
    In commit de98f7a ("TI-Integration: usb: musb: fix miss merge in musb_core")
    I fixed up the missmerge. Before that, commit 90c17b09d ("usb: musb:
    musb_core: Destroy dma controller only once") tried to do the same and
    removed the same function from musb_remove().
    
    Now after a rmmod + modprobe combo with don't get a dma engine and
    driver won't load.
    Thie patch reverts the first fix and now we are very close to mainline.
    musb_remove() used to be called from the cleanup error path as well so
    we should avoid this (even this tree does not do this for some reason).
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -2096,6 +2096,8 @@ static int musb_remove(struct platform_device *pdev)
 	musb_exit_debugfs(musb);
 	musb_shutdown(pdev);
 
+	if (musb->dma_controller)
+		dma_controller_destroy(musb->dma_controller);
 
 	musb_free(musb);
 	device_init_wakeup(dev, 0);
