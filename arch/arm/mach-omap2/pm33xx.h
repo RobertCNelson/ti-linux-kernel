2fa04ac941f2ef31670f284c902312b26557500d
Dave Gerlach <d-gerlach@ti.com>
2014-02-01 15:57:44 -0600

    ARM: AM43XX: Bump firmware requirement for DDR3 DS0
    CM3 firmware version 0x185 is required for AM43XX DDR3 DeepSleep0 support
    due to the need for IO isolation to toggle the VTT regulator control on
    GPIO5_7.
    
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>

--- arch/arm/mach-omap2/pm33xx.h
+++ arch/arm/mach-omap2/pm33xx.h
@@ -61,7 +61,7 @@ int am33xx_do_sram_cpuidle(u32, u32);
 #define IPC_CMD_RESET			0xe
 #define DS_IPC_DEFAULT			0xffffffff
 #define M3_VERSION_UNKNOWN		0x0000ffff
-#define M3_BASELINE_VERSION		0x183
+#define M3_BASELINE_VERSION		0x185
 
 #define M3_STATE_UNKNOWN		0
 #define M3_STATE_RESET			1
