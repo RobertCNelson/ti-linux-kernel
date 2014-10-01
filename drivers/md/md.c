fca918cf521ed7b6676212fd5a426d1f133be774
NeilBrown <neilb@suse.de>
2014-10-01 15:55:31 +1000

    md: remove need for mddev_lock() in md_seq_show()
    The only access in md_seq_show that could suffer from races
    not protected by ->lock is walking the rdev list.
    This can receive sufficient protection from 'rcu'.
    
    So use rdev_for_each_rcu() and get rid of mddev_lock().
    
    Now reading /proc/mdstat will never block in md_seq_show.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -6880,9 +6880,6 @@ static int md_seq_show(struct seq_file *seq, void *v)
 		return 0;
 	}
 
-	if (mddev_lock(mddev) < 0)
-		return -EINTR;
-
 	spin_lock(&mddev->lock);
 	if (mddev->pers || mddev->raid_disks || !list_empty(&mddev->disks)) {
 		seq_printf(seq, "%s : %sactive", mdname(mddev),
@@ -6896,7 +6893,8 @@ static int md_seq_show(struct seq_file *seq, void *v)
 		}
 
 		sectors = 0;
-		rdev_for_each(rdev, mddev) {
+		rcu_read_lock();
+		rdev_for_each_rcu(rdev, mddev) {
 			char b[BDEVNAME_SIZE];
 			seq_printf(seq, " %s[%d]",
 				bdevname(rdev->bdev,b), rdev->desc_nr);
@@ -6912,6 +6910,7 @@ static int md_seq_show(struct seq_file *seq, void *v)
 				seq_printf(seq, "(R)");
 			sectors += rdev->sectors;
 		}
+		rcu_read_unlock();
 
 		if (!list_empty(&mddev->disks)) {
 			if (mddev->pers)
@@ -6955,7 +6954,6 @@ static int md_seq_show(struct seq_file *seq, void *v)
 		seq_printf(seq, "\n");
 	}
 	spin_unlock(&mddev->lock);
-	mddev_unlock(mddev);
 
 	return 0;
 }
