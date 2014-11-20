c3dfc6d382a3d1b621b12f9c6936ee9e0d17a0c4
Andy Lutomirski <luto@amacapital.net>
2014-11-19 17:41:09 -0800

    x86, traps: Track entry into and exit from IST context
    We currently pretend that IST context is like standard exception
    context, but this is incorrect.  IST entries from userspace are like
    standard exceptions except that they use per-cpu stacks, so they are
    atomic.  IST entries from kernel space are like NMIs from RCU's
    perspective -- they are not quiescent states even if they
    interrupted the kernel during a quiescent state.
    
    Add and use ist_enter and ist_exit to track IST context.  Even
    though x86_32 has no IST stacks, we track these interrupts the same
    way.
    
    This fixes two issues:
    
     - Scheduling from an IST interrupt handler will now warn.  It would
       previously appear to work as long as we got lucky and nothing
       overwrote the stack frame.  (I don't know of any bugs in this
       that would trigger the warning, but it's good to be on the safe
       side.)
    
     - RCU handling in IST context was dangerous.  As far as I know,
       only machine checks were likely to trigger this, but it's good to
       be on the safe side.
    
    Note that the machine check handlers appears to have been missing
    any context tracking at all before this patch.
    
    Cc: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    Cc: Josh Triplett <josh@joshtriplett.org>
    Cc: Frédéric Weisbecker <fweisbec@gmail.com>
    Signed-off-by: Andy Lutomirski <luto@amacapital.net>

--- arch/x86/kernel/cpu/mcheck/mce.c
+++ arch/x86/kernel/cpu/mcheck/mce.c
@@ -43,6 +43,7 @@
 #include <linux/export.h>
 
 #include <asm/processor.h>
+#include <asm/traps.h>
 #include <asm/mce.h>
 #include <asm/msr.h>
 
@@ -1016,6 +1017,7 @@ void do_machine_check(struct pt_regs *regs, long error_code)
 {
 	struct mca_config *cfg = &mca_cfg;
 	struct mce m, *final;
+	enum ctx_state prev_state;
 	int i;
 	int worst = 0;
 	int severity;
@@ -1037,6 +1039,8 @@ void do_machine_check(struct pt_regs *regs, long error_code)
 	DECLARE_BITMAP(toclear, MAX_NR_BANKS);
 	DECLARE_BITMAP(valid_banks, MAX_NR_BANKS);
 	char *msg = "Unknown";
+
+	prev_state = ist_enter(regs);
 
 	this_cpu_inc(mce_exception_count);
 
@@ -1168,6 +1172,7 @@ void do_machine_check(struct pt_regs *regs, long error_code)
 	mce_wrmsrl(MSR_IA32_MCG_STATUS, 0);
 out:
 	sync_core();
+	ist_exit(regs, prev_state);
 }
 EXPORT_SYMBOL_GPL(do_machine_check);
 
