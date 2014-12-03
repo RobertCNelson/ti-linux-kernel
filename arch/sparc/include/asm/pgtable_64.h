0f9aba275f900808063f996fa9b7a88ff30ea7c9
Kirill A. Shutemov <kirill@shutemov.name>
2014-12-03 18:54:22 +1100

    mm-fix-huge-zero-page-accounting-in-smaps-report-fix
    Return type should be 'unsigned long'.
    
    Andrew, please fold this into the patch:
    
    Reported-by: Wu Fengguang <fengguang.wu@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/sparc/include/asm/pgtable_64.h
+++ arch/sparc/include/asm/pgtable_64.h
@@ -667,7 +667,7 @@ static inline unsigned long pmd_pfn(pmd_t pmd)
 }
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
-static inline pmd_t pmd_dirty(pmd_t pmd)
+static inline unsigned long pmd_dirty(pmd_t pmd)
 {
 	pte_t pte = __pte(pmd_val(pmd));
 
