cccae108896fdadbe02f2758caf8282589bb783c
Andrew Morton <akpm@linux-foundation.org>
2014-12-03 18:54:23 +1100

    mm-fix-huge-zero-page-accounting-in-smaps-report-fix-2-fix
    use do_div to fix 32-bit build
    
    fs/built-in.o: In function `smaps_account':
    task_mmu.c:(.text+0x943a3): undefined reference to `__udivdi3'
    
    Cc: "Kirill A. Shutemov" <kirill@shutemov.name>
    Cc: Wu Fengguang <fengguang.wu@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -461,11 +461,15 @@ static void smaps_account(struct mem_size_stats *mss, struct page *page,
 		mss->referenced += size;
 	mapcount = page_mapcount(page);
 	if (mapcount >= 2) {
+		u64 pss_delta;
+
 		if (dirty || PageDirty(page))
 			mss->shared_dirty += size;
 		else
 			mss->shared_clean += size;
-		mss->pss += ((u64)size << PSS_SHIFT) / mapcount;
+		pss_delta = (u64)size << PSS_SHIFT;
+		do_div(pss_delta, mapcount);
+		mss->pss += pss_delta;
 	} else {
 		if (dirty || PageDirty(page))
 			mss->private_dirty += size;
