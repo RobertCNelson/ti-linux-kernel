708d84b730bd19a5ff9a81a32d3d173f9d4c3ff4
Andrew Morton <akpm@linux-foundation.org>
2015-01-24 13:10:23 +1100

    mm-fix-xip-fault-vs-truncate-race-fix-fix
    Cc: Matthew Wilcox <matthew.r.wilcox@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -1301,11 +1301,6 @@ static void unmap_single_vma(struct mmu_gather *tlb,
 			 * safe to do nothing in this case.
 			 */
 			if (vma->vm_file) {
-				/*
-				 * Note that DAX uses i_mmap_lock to serialise
-				 * against file truncate - truncate calls into
-				 * unmap_single_vma().
-				 */
 				i_mmap_lock_write(vma->vm_file->f_mapping);
 				__unmap_hugepage_range_final(tlb, vma, start, end, NULL);
 				i_mmap_unlock_write(vma->vm_file->f_mapping);
@@ -2332,6 +2327,7 @@ void unmap_mapping_range(struct address_space *mapping,
 		details.last_index = ULONG_MAX;
 
 
+	/* DAX uses i_mmap_lock to serialise file truncate vs page fault */
 	i_mmap_lock_write(mapping);
 	if (unlikely(!RB_EMPTY_ROOT(&mapping->i_mmap)))
 		unmap_mapping_range_tree(&mapping->i_mmap, &details);
