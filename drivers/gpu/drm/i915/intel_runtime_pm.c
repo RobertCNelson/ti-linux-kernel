12f9d889258935893252422f2bb5968a42a7b7e8
Damien Lespiau <damien.lespiau@intel.com>
2015-03-02 18:20:31 +0000

    drm/i915/skl: Restore the DDI translation tables when enabling PW1
    I was dumping the DDI translation tables to make sure my patch updating
    the HDMI entry was doing the right thing when I noticed that the table
    was showing reset values after DPMS.
    
    And indeed, the DDI translation registers are in power well 1 on SKL,
    and so we're losing their values when shutting down eDP.
    
    Calling intel_prepare_ddi() on PW1 enabling re-programs the table.
    
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>
    Reviewed-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_runtime_pm.c
+++ drivers/gpu/drm/i915/intel_runtime_pm.c
@@ -194,8 +194,10 @@ static void hsw_power_well_post_enable(struct drm_i915_private *dev_priv)
 	outb(inb(VGA_MSR_READ), VGA_MSR_WRITE);
 	vga_put(dev->pdev, VGA_RSRC_LEGACY_IO);
 
-	if (IS_BROADWELL(dev) || (INTEL_INFO(dev)->gen >= 9))
+	if (IS_BROADWELL(dev) || (INTEL_INFO(dev)->gen >= 9)) {
+		intel_prepare_ddi(dev);
 		gen8_irq_power_well_post_enable(dev_priv);
+	}
 }
 
 static void hsw_set_power_well(struct drm_i915_private *dev_priv,
