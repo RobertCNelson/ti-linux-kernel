c49d7b823ffa738ffa7839ea0729b69c048e3296
Toshi Kani <toshi.kani@hp.com>
2015-03-05 08:44:06 -0700

    Fix undefined ioremap_huge_init when CONFIG_MMU is not set
    Fix a build error, undefined reference to ioremap_huge_init, when
    CONFIG_MMU is not defined on linux-next and -mm tree.
    
    lib/ioremap.o is not linked to the kernel when CONFIG_MMU is not
    defined.
    
    Signed-off-by: Toshi Kani <toshi.kani@hp.com>

--- include/linux/io.h
+++ include/linux/io.h
@@ -38,11 +38,12 @@ static inline int ioremap_page_range(unsigned long addr, unsigned long end,
 }
 #endif
 
-void __init ioremap_huge_init(void);
-
 #ifdef CONFIG_HAVE_ARCH_HUGE_VMAP
+void __init ioremap_huge_init(void);
 int arch_ioremap_pud_supported(void);
 int arch_ioremap_pmd_supported(void);
+#else
+static inline void ioremap_huge_init(void) { }
 #endif
 
 /*
