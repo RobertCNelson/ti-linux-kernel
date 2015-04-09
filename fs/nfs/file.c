f52cbe4c9838a0ce65376680f52e074bf0a66748
Al Viro <viro@zeniv.linux.org.uk>
2015-04-09 14:11:08 -0400

    nfs: generic_write_checks() shouldn't be done on swapout...
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/nfs/file.c
+++ fs/nfs/file.c
@@ -680,8 +680,12 @@ ssize_t nfs_file_write(struct kiocb *iocb, struct iov_iter *from)
 	if (result)
 		return result;
 
-	if (iocb->ki_flags & IOCB_DIRECT)
-		return nfs_file_direct_write(iocb, from, pos);
+	if (iocb->ki_flags & IOCB_DIRECT) {
+		result = generic_write_checks(iocb, from);
+		if (result <= 0)
+			return result;
+		return nfs_file_direct_write(iocb, from);
+	}
 
 	dprintk("NFS: write(%pD2, %zu@%Ld)\n",
 		file, count, (long long) pos);
