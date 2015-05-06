7dffa13d1f324f51d032e79ef5cbb56e4d4828e1
Andrew Morton <akpm@linux-foundation.org>
2015-05-06 10:16:49 +1000

    slub-bulk-allocation-from-per-cpu-partial-pages-fix
    fix comments, code layout
    
    Cc: Christoph Lameter <cl@linux.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Jesper Dangaard Brouer <brouer@redhat.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/slub.c
+++ mm/slub.c
@@ -2771,17 +2771,16 @@ bool kmem_cache_alloc_bulk(struct kmem_cache *s, gfp_t flags, size_t size,
 
 			if (unlikely(!object)) {
 				/*
-				 * Check if there remotely freed objects
-				 * availalbe in the page.
+				 * Check if there are remotely freed objects
+				 * available in the page.
 				 */
 				object = get_freelist(s, c->page);
 
 				if (!object) {
 					/*
-					 * All objects in use lets check if
-					 * we have other per cpu partial
-					 * pages that have available
-					 * objects.
+					 * All objects in use.  Let's check if
+					 * we have other per cpu partial pages
+					 * that have available objects.
 					 */
 					c->page = c->partial;
 					if (!c->page) {
@@ -2792,13 +2791,11 @@ bool kmem_cache_alloc_bulk(struct kmem_cache *s, gfp_t flags, size_t size,
 
 					/* Next per cpu partial page */
 					c->partial = c->page->next;
-					c->freelist = get_freelist(s,
-							c->page);
+					c->freelist = get_freelist(s, c->page);
 					continue;
 				}
 
 			}
-
 
 			*p++ = object;
 			size--;
