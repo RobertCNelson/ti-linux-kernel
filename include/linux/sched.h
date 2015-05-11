a30464c7f712915bfdf5b79e9c7be7195dd7ce19
David Hildenbrand <dahi@linux.vnet.ibm.com>
2015-05-11 17:52:06 +0200

    sched/preempt, mm/fault: Count pagefault_disable() levels in pagefault_disabled
    Until now, pagefault_disable()/pagefault_enabled() used the preempt
    count to track whether in an environment with pagefaults disabled (can
    be queried via in_atomic()).
    
    This patch introduces a separate counter in task_struct to count the
    level of pagefault_disable() calls. We'll keep manipulating the preempt
    count to retain compatibility to existing pagefault handlers.
    
    It is now possible to verify whether in a pagefault_disable() envionment
    by calling pagefault_disabled(). In contrast to in_atomic() it will not
    be influenced by preempt_enable()/preempt_disable().
    
    This patch is based on a patch from Ingo Molnar.
    
    [upstream commit 8bcbde5480f9777f8b74d71493722c663e22c21b]
    Signed-off-by: David Hildenbrand <dahi@linux.vnet.ibm.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1726,6 +1726,7 @@ struct task_struct {
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
 	unsigned long	task_state_change;
 #endif
+	int pagefault_disabled;
 };
 
 /* Future-safe accessor for struct task_struct's cpus_allowed. */
