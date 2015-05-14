618ba914137d7452a103d6131034c11366961594
labbott <labbott@redhat.com>
2015-05-14 19:31:58 +0100

    ARM: 8358/1: Don't use memblock limit for the lowmem bound
    The memblock limit is currently used in find_limits
    to find the bounds for ZONE_NORMAL. The memblock
    limit may need to be rounded down a PMD size to ensure
    allocations are fully mapped though. This has the side
    effect of reducing the amount of memory in ZONE_NORMAL.
    Since we generally want to optimize for more lowmem, fix
    this by using arm_lowmem_limit to calculate the bounds.
    This what is used for actually mapping lowmem anyway.
    
    Before:
    # cat /proc/zoneinfo | grep managed
            managed  62920
    
    After:
    # cat /proc/zoneinfo | grep managed
            managed  63336
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Tested-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Laura Abbott <labbott@redhat.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -89,7 +89,7 @@ __tagtable(ATAG_INITRD2, parse_tag_initrd2);
 static void __init find_limits(unsigned long *min, unsigned long *max_low,
 			       unsigned long *max_high)
 {
-	*max_low = PFN_DOWN(memblock_get_current_limit());
+	*max_low = PFN_DOWN(arm_lowmem_limit);
 	*min = PFN_UP(memblock_start_of_DRAM());
 	*max_high = PFN_DOWN(memblock_end_of_DRAM());
 }
