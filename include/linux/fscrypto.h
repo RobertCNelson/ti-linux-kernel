c5aba29e282b41f4d92556ea0769c0466bbe922f
Jaegeuk Kim <jaegeuk@kernel.org>
2015-05-15 16:32:57 -0700

    fs crypto: add crypto.c for encrypt/decrypt functions
    This patch adds crypto.c supporting encrypting and decrypting functions.
    
    1. IO preparation:
      - fscrypt_get_ctx / fscrypt_release_ctx
    
    2. before IOs:
      - fscrypt_encrypt_page
      - fscrypt_decrypt_page
      - fscrypt_zeroout_range
    
    3. after IOs:
      - fscrypt_decrypt_bio_pages
      - fscrypt_pullback_bio_page
      - fscrypt_restore_control_page
    
    Signed-off-by: Michael Halcrow <mhalcrow@google.com>
    Signed-off-by: Ildar Muslukhov <ildarm@google.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/linux/fscrypto.h
+++ include/linux/fscrypto.h
@@ -236,4 +236,63 @@ static inline int fscrypt_has_encryption_key(struct inode *inode)
 	return 0;
 #endif
 }
+
+/* crypto.c */
+extern struct kmem_cache *fscrypt_info_cachep;
+int fscrypt_initialize(void);
+
+#ifdef CONFIG_FS_ENCRYPTION
+/* crypto.c */
+struct fscrypt_ctx *fscrypt_get_ctx(struct inode *);
+void fscrypt_release_ctx(struct fscrypt_ctx *);
+struct page *fscrypt_encrypt_page(struct inode *, struct page *);
+int fscrypt_decrypt_page(struct page *);
+void fscrypt_decrypt_bio_pages(struct fscrypt_ctx *, struct bio *);
+void fscrypt_pullback_bio_page(struct page **, bool);
+void fscrypt_restore_control_page(struct page *);
+int fscrypt_zeroout_range(struct inode *, pgoff_t, sector_t, unsigned int);
+#else
+/* crypto.c */
+static inline struct fscrypt_ctx *fscrypt_get_ctx(struct inode *i)
+{
+	return ERR_PTR(-EOPNOTSUPP);
+}
+
+static inline void fscrypt_release_ctx(struct fscrypt_ctx *c)
+{
+	return;
+}
+
+static inline struct page *fscrypt_encrypt_page(struct inode *i, struct page *p)
+{
+	return ERR_PTR(-EOPNOTSUPP);
+}
+
+static inline int fscrypt_decrypt_page(struct page *p)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void fscrypt_decrypt_bio_pages(struct fscrypt_ctx *c,
+						struct bio *b)
+{
+	return;
+}
+
+static inline void fscrypt_pullback_bio_page(struct page **p, bool b)
+{
+	return;
+}
+
+static inline void fscrypt_restore_control_page(struct page *p)
+{
+	return;
+}
+
+static inline int fscrypt_zeroout_range(struct inode *i, pgoff_t p,
+					sector_t s, unsigned int f)
+{
+	return -EOPNOTSUPP;
+}
+#endif
 #endif	/* _LINUX_FSCRYPTO_H */
