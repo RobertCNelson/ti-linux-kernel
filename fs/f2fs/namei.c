8906e0ea7f7d93f5619ac03402c9c01bb3091830
Jaegeuk Kim <jaegeuk@kernel.org>
2015-05-19 16:11:40 -0700

    f2fs crypto: check encryption for tmpfile
    This patch adds to check encryption for tmpfile in early stage.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -569,6 +569,10 @@ out:
 
 static int f2fs_tmpfile(struct inode *dir, struct dentry *dentry, umode_t mode)
 {
+	int err = f2fs_get_encryption_info(dir);
+	if (err)
+		return err;
+
 	return __f2fs_tmpfile(dir, dentry, mode, NULL);
 }
 
