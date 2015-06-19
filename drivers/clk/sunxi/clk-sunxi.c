eff5a8cb9c3ff088ce35d4cd414fdb4d22b40f4f
Stephen Boyd <sboyd@codeaurora.org>
2015-06-19 15:00:46 -0700

    clk: sunxi: Include clk.h
    This clock provider uses the consumer API, so include clk.h
    explicitly.
    
    Cc: Chen-Yu Tsai <wens@csie.org>
    Cc: Maxime Ripard <maxime.ripard@free-electrons.com>
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- drivers/clk/sunxi/clk-sunxi.c
+++ drivers/clk/sunxi/clk-sunxi.c
@@ -14,6 +14,8 @@
  * GNU General Public License for more details.
  */
 
+#include <linux/slab.h>
+#include <linux/clk.h>
 #include <linux/clk-provider.h>
 #include <linux/clkdev.h>
 #include <linux/of.h>
