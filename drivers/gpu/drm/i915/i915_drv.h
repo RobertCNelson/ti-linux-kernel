7616d2410010bddfa8418f4a820f1ecc2ab2cbbf
Chris Wilson <chris@chris-wilson.co.uk>
2015-08-26 12:55:57 +0100

    drm/i915: Set the map-and-fenceable flag for preallocated objects
    As we mark the preallocated objects as bound, we should also flag them
    correctly as being map-and-fenceable (if appropriate!) so that latter
    users do not get confused and try and rebind the pinned vma in order to
    get a map-and-fenceable binding.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: "Goel, Akash" <akash.goel@intel.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Jesse Barnes <jbarnes@virtuousgeek.org>
    Cc: stable@vger.kernel.org
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2800,6 +2800,7 @@ i915_gem_object_ggtt_pin(struct drm_i915_gem_object *obj,
 
 int i915_vma_bind(struct i915_vma *vma, enum i915_cache_level cache_level,
 		  u32 flags);
+void __i915_vma_set_map_and_fenceable(struct i915_vma *vma);
 int __must_check i915_vma_unbind(struct i915_vma *vma);
 int i915_gem_object_put_pages(struct drm_i915_gem_object *obj);
 void i915_gem_release_all_mmaps(struct drm_i915_private *dev_priv);
