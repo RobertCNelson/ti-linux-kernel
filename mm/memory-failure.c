eb3e08678ccdcd494977cb229e1d07fba13563b0
Andrew Morton <akpm@linux-foundation.org>
2016-01-13 16:16:26 +1100

    mm-soft-offline-exit-with-failure-for-non-anonymous-thp-fix
    fix build
    
    Cc: "Kirill A. Shutemov" <kirill@shutemov.name>
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -1694,9 +1694,9 @@ static int soft_offline_in_use_page(struct page *page, int flags)
 		if (!PageAnon(hpage) || unlikely(split_huge_page(hpage))) {
 			unlock_page(hpage);
 			if (!PageAnon(hpage))
-				pr_info("soft offline: %#lx: non anonymous thp\n", pfn);
+				pr_info("soft offline: %#lx: non anonymous thp\n", page_to_pfn(page));
 			else
-				pr_info("soft offline: %#lx: thp split failed\n", pfn);
+				pr_info("soft offline: %#lx: thp split failed\n", page_to_pfn(page));
 			put_hwpoison_page(hpage);
 			return -EBUSY;
 		}
