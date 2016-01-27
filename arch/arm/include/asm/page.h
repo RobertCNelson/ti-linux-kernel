3c98795f6affb7133106604a1891c7d1b7fd3b79
Andrew Morton <akpm@linux-foundation.org>
2016-01-28 09:47:04 +1100

    arm-arch-arm-include-asm-pageh-needs-personalityh-fix
    assembler not impressed being fed personality.h
    
    Cc: Konstantin Khlebnikov <koct9i@gmail.com>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/arm/include/asm/page.h
+++ arch/arm/include/asm/page.h
@@ -10,14 +10,14 @@
 #ifndef _ASMARM_PAGE_H
 #define _ASMARM_PAGE_H
 
-#include <linux/personality.h>	/* For READ_IMPLIES_EXEC */
-
 /* PAGE_SHIFT determines the page size */
 #define PAGE_SHIFT		12
 #define PAGE_SIZE		(_AC(1,UL) << PAGE_SHIFT)
 #define PAGE_MASK		(~((1 << PAGE_SHIFT) - 1))
 
 #ifndef __ASSEMBLY__
+
+#include <linux/personality.h>	/* For READ_IMPLIES_EXEC */
 
 #ifndef CONFIG_MMU
 
