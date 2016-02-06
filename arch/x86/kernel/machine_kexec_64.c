c17628319d69cf4cadcd8c044e42bec66ca0f3c2
Xunlei Pang <xlpang@redhat.com>
2016-02-06 12:16:25 +1100

    kexec-provide-arch_kexec_protectunprotect_crashkres-v4
    v3->v4:
    Add some comments according to Minfei's suggestion.
    
    Signed-off-by: Xunlei Pang <xlpang@redhat.com>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: Minfei Huang <mhuang@redhat.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/kernel/machine_kexec_64.c
+++ arch/x86/kernel/machine_kexec_64.c
@@ -539,18 +539,21 @@ overflow:
 }
 #endif /* CONFIG_KEXEC_FILE */
 
-#ifdef CONFIG_KEXEC_CORE
 static int
 kexec_mark_range(unsigned long start, unsigned long end, bool protect)
 {
 	struct page *page;
 	unsigned int nr_pages;
 
-	if (!start || !end || start >= end)
+	/*
+	 * For physical range: [start, end]. We must skip the unassigned
+	 * crashk resource with zero-valued "end" member.
+	 */
+	if (!end || start > end)
 		return 0;
 
 	page = pfn_to_page(start >> PAGE_SHIFT);
-	nr_pages = (end + 1 - start) >> PAGE_SHIFT;
+	nr_pages = (end >> PAGE_SHIFT) - (start >> PAGE_SHIFT) + 1;
 	if (protect)
 		return set_pages_ro(page, nr_pages);
 	else
@@ -566,9 +569,9 @@ static void kexec_mark_crashkres(bool protect)
 	/* Don't touch the control code page used in crash_kexec().*/
 	control = PFN_PHYS(page_to_pfn(kexec_crash_image->control_code_page));
 	/* Control code page is located in the 2nd page. */
-	control = control + PAGE_SIZE;
-	kexec_mark_range(crashk_res.start, control - 1, protect);
-	kexec_mark_range(control + PAGE_SIZE, crashk_res.end, protect);
+	kexec_mark_range(crashk_res.start, control + PAGE_SIZE - 1, protect);
+	control += KEXEC_CONTROL_PAGE_SIZE;
+	kexec_mark_range(control, crashk_res.end, protect);
 }
 
 void arch_kexec_protect_crashkres(void)
@@ -580,4 +583,3 @@ void arch_kexec_unprotect_crashkres(void)
 {
 	kexec_mark_crashkres(false);
 }
-#endif
