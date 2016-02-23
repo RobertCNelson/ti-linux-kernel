362977b0dbc624697a9dfbe557d62f17422852c6
Jaegeuk Kim <jaegeuk@kernel.org>
2016-02-23 09:21:37 -0800

    f2fs crypto: sync ext4_lookup and ext4_file_open
    This patch tries to catch up with lookup and open policies in ext4.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -855,7 +855,7 @@ static int f2fs_readdir(struct file *file, struct dir_context *ctx)
 
 	if (f2fs_encrypted_inode(inode)) {
 		err = fscrypt_get_encryption_info(inode);
-		if (err && err != -EOPNOTSUPP)
+		if (err && err != -ENOKEY && err != -EOPNOTSUPP)
 			return err;
 
 		err = fscrypt_fname_alloc_buffer(inode, F2FS_NAME_LEN, &fstr);
