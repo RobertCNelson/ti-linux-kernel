ae2c82827e558fd8d3f19ec6b803cf5b7b3c64c2
Vlastimil Babka <vbabka@suse.cz>
2016-03-09 11:10:37 +1100

    mm-kswapd-replace-kswapd-compaction-with-waking-up-kcompactd-fix
    Change zone_balanced() check in balance_pgdat() to consider only base
    pages as that's the same what kswapd_shrink_zone() later does.  So a zone
    that would be selected due to lack of high-order pages would not be
    reclaimed anyway, and just cause extra CPU churn.
    
    Also remove unnecessary variable testorder in kswapd_shrink_zone().
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Reported-by: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3090,7 +3090,6 @@ static bool kswapd_shrink_zone(struct zone *zone,
 			       int classzone_idx,
 			       struct scan_control *sc)
 {
-	int testorder = sc->order;
 	unsigned long balance_gap;
 	bool lowmem_pressure;
 
@@ -3111,7 +3110,7 @@ static bool kswapd_shrink_zone(struct zone *zone,
 	 * reclaim is necessary
 	 */
 	lowmem_pressure = (buffer_heads_over_limit && is_highmem(zone));
-	if (!lowmem_pressure && zone_balanced(zone, testorder, false,
+	if (!lowmem_pressure && zone_balanced(zone, sc->order, false,
 						balance_gap, classzone_idx))
 		return true;
 
@@ -3126,7 +3125,7 @@ static bool kswapd_shrink_zone(struct zone *zone,
 	 * waits.
 	 */
 	if (zone_reclaimable(zone) &&
-	    zone_balanced(zone, testorder, false, 0, classzone_idx)) {
+	    zone_balanced(zone, sc->order, false, 0, classzone_idx)) {
 		clear_bit(ZONE_CONGESTED, &zone->flags);
 		clear_bit(ZONE_DIRTY, &zone->flags);
 	}
@@ -3207,7 +3206,7 @@ static int balance_pgdat(pg_data_t *pgdat, int order, int classzone_idx)
 				break;
 			}
 
-			if (!zone_balanced(zone, order, true, 0, 0)) {
+			if (!zone_balanced(zone, order, false, 0, 0)) {
 				end_zone = i;
 				break;
 			} else {
