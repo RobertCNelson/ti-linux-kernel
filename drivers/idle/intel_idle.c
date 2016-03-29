7711299ae95343100004eeae84392df84b41b281
Richard Cochran <rcochran@linutronix.de>
2016-03-29 15:15:58 +0200

    intel_idle: Avoid a double free of the per-CPU data.
    The helper function, intel_idle_cpuidle_devices_uninit, frees the
    globally allocated per-CPU data.  However, this function is invoked
    from the hot plug notifier callback at a time when freeing that data
    is not safe.
    
    If the call to cpuidle_register_driver() should fail (say, due to lack
    of memory), then the driver will free its per-CPU region.  On the
    *next* CPU_ONLINE event, the driver will happily use the region again
    and even free it again if the failure repeats.
    
    This patch fixes the issue by moving the call to free_percpu() outside
    of the helper function at the two call sites that actually need to
    free the per-CPU data.
    
    Signed-off-by: Richard Cochran <rcochran@linutronix.de>
    Signed-off-by: Len Brown <len.brown@intel.com>

--- drivers/idle/intel_idle.c
+++ drivers/idle/intel_idle.c
@@ -1069,7 +1069,7 @@ static int __init intel_idle_probe(void)
 
 /*
  * intel_idle_cpuidle_devices_uninit()
- * unregister, free cpuidle_devices
+ * Unregisters the cpuidle devices.
  */
 static void intel_idle_cpuidle_devices_uninit(void)
 {
@@ -1080,9 +1080,6 @@ static void intel_idle_cpuidle_devices_uninit(void)
 		dev = per_cpu_ptr(intel_idle_cpuidle_devices, i);
 		cpuidle_unregister_device(dev);
 	}
-
-	free_percpu(intel_idle_cpuidle_devices);
-	return;
 }
 
 /*
@@ -1347,6 +1344,7 @@ static int __init intel_idle_init(void)
 		if (retval) {
 			cpu_notifier_register_done();
 			cpuidle_unregister_driver(&intel_idle_driver);
+			free_percpu(intel_idle_cpuidle_devices);
 			return retval;
 		}
 	}
@@ -1369,6 +1367,7 @@ static void __exit intel_idle_exit(void)
 	cpu_notifier_register_done();
 
 	cpuidle_unregister_driver(&intel_idle_driver);
+	free_percpu(intel_idle_cpuidle_devices);
 }
 
 module_init(intel_idle_init);
