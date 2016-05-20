f67e51747cf1916b9b11504404d59d525786242f
Ebru Akagunduz <ebru.akagunduz@gmail.com>
2016-05-20 13:04:39 +1000

    mm, thp: avoid unnecessary swapin in khugepaged
    Currently khugepaged makes swapin readahead to improve THP collapse rate.
    This patch checks vm statistics to avoid workload of swapin, if
    unnecessary.  So that when system under pressure, khugepaged won't consume
    resources to swapin and won't trigger direct reclaim when swapin
    readahead.
    
    The patch was tested with a test program that allocates 800MB of memory,
    writes to it, and then sleeps.  The system was forced to swap out all.
    Afterwards, the test program touches the area by writing, it skips a page
    in each 20 pages of the area.  When waiting to swapin readahead left part
    of the test, the memory forced to be busy doing page reclaim.  There was
    enough free memory during test, khugepaged did not swapin readahead due to
    business.
    
    Test results:
    
                            After swapped out
    -------------------------------------------------------------------
                  | Anonymous | AnonHugePages | Swap      | Fraction  |
    -------------------------------------------------------------------
    With patch    | 0 kB      |  0 kB         | 800000 kB |    %100   |
    -------------------------------------------------------------------
    Without patch | 0 kB      |  0 kB         | 800000 kB |    %100   |
    -------------------------------------------------------------------
    
                            After swapped in
    -------------------------------------------------------------------
                  | Anonymous | AnonHugePages | Swap      | Fraction  |
    -------------------------------------------------------------------
    With patch    | 385120 kB | 102400 kB     | 414880 kB |    %26    |
    -------------------------------------------------------------------
    Without patch | 389728 kB | 194560 kB     | 410272 kB |    %49    |
    -------------------------------------------------------------------
    
    Signed-off-by: Ebru Akagunduz <ebru.akagunduz@gmail.com>
    Acked-by: Rik van Riel <riel@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Boaz Harrosh <boaz@plexistor.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -103,6 +103,7 @@ static DECLARE_WAIT_QUEUE_HEAD(khugepaged_wait);
  */
 static unsigned int khugepaged_max_ptes_none __read_mostly;
 static unsigned int khugepaged_max_ptes_swap __read_mostly = HPAGE_PMD_NR/8;
+static unsigned long allocstall;
 
 static int khugepaged(void *none);
 static int khugepaged_slab_init(void);
@@ -2427,7 +2428,7 @@ static void collapse_huge_page(struct mm_struct *mm,
 	struct page *new_page;
 	spinlock_t *pmd_ptl, *pte_ptl;
 	int isolated = 0, result = 0;
-	unsigned long hstart, hend;
+	unsigned long hstart, hend, swap, curr_allocstall;
 	struct mem_cgroup *memcg;
 	unsigned long mmun_start;	/* For mmu_notifiers */
 	unsigned long mmun_end;		/* For mmu_notifiers */
@@ -2482,7 +2483,14 @@ static void collapse_huge_page(struct mm_struct *mm,
 		goto out;
 	}
 
-	__collapse_huge_page_swapin(mm, vma, address, pmd);
+	swap = get_mm_counter(mm, MM_SWAPENTS);
+	curr_allocstall = sum_vm_event(ALLOCSTALL);
+	/*
+	 * When system under pressure, don't swapin readahead.
+	 * So that avoid unnecessary resource consuming.
+	 */
+	if (allocstall == curr_allocstall && swap != 0)
+		__collapse_huge_page_swapin(mm, vma, address, pmd);
 
 	anon_vma_lock_write(vma->anon_vma);
 
@@ -2885,6 +2893,7 @@ static void khugepaged_wait_work(void)
 		if (!scan_sleep_jiffies)
 			return;
 
+		allocstall = sum_vm_event(ALLOCSTALL);
 		khugepaged_sleep_expire = jiffies + scan_sleep_jiffies;
 		wait_event_freezable_timeout(khugepaged_wait,
 					     khugepaged_should_wakeup(),
@@ -2892,8 +2901,10 @@ static void khugepaged_wait_work(void)
 		return;
 	}
 
-	if (khugepaged_enabled())
+	if (khugepaged_enabled()) {
+		allocstall = sum_vm_event(ALLOCSTALL);
 		wait_event_freezable(khugepaged_wait, khugepaged_wait_event());
+	}
 }
 
 static int khugepaged(void *none)
@@ -2902,6 +2913,7 @@ static int khugepaged(void *none)
 
 	set_freezable();
 	set_user_nice(current, MAX_NICE);
+	allocstall = sum_vm_event(ALLOCSTALL);
 
 	while (!kthread_should_stop()) {
 		khugepaged_do_scan();
