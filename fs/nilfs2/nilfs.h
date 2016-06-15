b6566fc3024d71228a04a6f058ec45252753a2a9
Arnd Bergmann <arnd@arndb.de>
2016-06-16 09:44:39 +1000

    nilfs: avoid warning about unused variables
    The newly added nilfs_msg() infrastructure caused a warning in
    nilfs_clear_dirty_page() when it is disabled:
    
    fs/nilfs2/page.c: In function 'nilfs_clear_dirty_page':
    fs/nilfs2/page.c:402:22: error: unused variable 'sb' [-Werror=unused-variable]
      struct super_block *sb = inode->i_sb;
    
    We can avoid the warning by adding a cast to void for the first
    argument of the macro.
    
    Fixes: aede53d223f0 ("nilfs2: replace nilfs_warning() with nilfs_msg()")
    Link: http://lkml.kernel.org/r/20160615201945.3348205-1-arnd@arndb.de
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/nilfs2/nilfs.h
+++ fs/nilfs2/nilfs.h
@@ -317,7 +317,10 @@ void __nilfs_error(struct super_block *sb, const char *function,
 #else
 
 #define nilfs_msg(sb, level, fmt, ...)					\
-	no_printk(fmt, ##__VA_ARGS__)
+	do {								\
+		no_printk(fmt, ##__VA_ARGS__);				\
+		(void)(sb);						\
+	} while (0)
 #define nilfs_error(sb, fmt, ...)					\
 	do {								\
 		no_printk(fmt, ##__VA_ARGS__);				\
