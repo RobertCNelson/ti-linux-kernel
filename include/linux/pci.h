d0baec408685298bbb7da8f33c3fe58e0ed98199
Bjorn Helgaas <bhelgaas@google.com>
2016-06-17 15:23:52 -0500

    PCI: Document connection between pci_power_t and hardware PM capability
    The dev.pme_support field, pci_pm_init(), and pci_pme_capable() depend on
    the fact that the pci_power_t values (PCI_D0, PCI_D1, etc.) match the
    definition of the PME_Support field of the Power Management Capabilities
    register in the Power Management capability (see PCI Bus Power Management
    spec r1.2, sec 3.2.3).
    
    Add a note to this effect at the pci_power_t typedef.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -101,6 +101,10 @@ enum {
 	DEVICE_COUNT_RESOURCE = PCI_NUM_RESOURCES,
 };
 
+/*
+ * pci_power_t values must match the bits in the PME_Support field of the
+ * Power Management Capabilities register in the Power Management capability.
+ */
 typedef int __bitwise pci_power_t;
 
 #define PCI_D0		((pci_power_t __force) 0)
