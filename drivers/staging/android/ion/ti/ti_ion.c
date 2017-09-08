RobertCNelson/ti-linux-kernel

commit dcbb0ca2550f4984cc95e81eda201e620aa8f7ee

TEMP: staging: android: ion: Add ion driver for TI platforms
Add ion support for Texas Instruments platforms.

Signed-off-by: Andrew F. Davis <afd@ti.com>
Acked-by: Tero Kristo <t-kristo@ti.com>

drivers/staging/android/ion/ti/ti_ion.c
@@ -1,0 +1,68 @@
+/*
+ * Texas Instruments ION Driver
+ *
+ * Copyright (C) 2017 Texas Instruments Incorporated - http://www.ti.com/
+ *	Andrew F. Davis <afd@ti.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/of.h>
+#include "../ion_priv.h"
+
+static struct ion_device *idev;
+static struct ion_heap *heap;
+
+static struct ion_platform_heap ti_ion_heap = {
+	.id	= ION_HEAP_TYPE_UNMAPPED,
+	.type	= ION_HEAP_TYPE_UNMAPPED,
+	.name	= "unmapped",
+	.base   = 0xbf300000,
+	.size   = 0x00400000,
+	.align	= SZ_4K,
+};
+
+static int __init ti_ion_init(void)
+{
+	struct device_node *node;
+
+	if (!of_have_populated_dt())
+		return -ENODEV;
+
+	node = of_find_node_by_path("/reserved-memory/secure_reserved");
+	if (!node)
+		return -ENODEV;
+
+	idev = ion_device_create(NULL);
+	if (IS_ERR(idev))
+		return PTR_ERR(idev);
+
+	heap = ion_heap_create(&ti_ion_heap);
+	if (IS_ERR_OR_NULL(heap)) {
+		ion_device_destroy(idev);
+		return PTR_ERR(heap);
+	}
+	ion_device_add_heap(idev, heap);
+
+	return 0;
+}
+module_init(ti_ion_init);
+
+static void __exit ti_ion_exit(void)
+{
+	ion_device_destroy(idev);
+	ion_heap_destroy(heap);
+}
+module_exit(ti_ion_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Andrew F. Davis <afd@ti.com>");
+MODULE_DESCRIPTION("Texas Instruments SDP ION Driver");

drivers/staging/android/ion/Kconfig
@@ -40,6 +40,12 @@ config ION_HISI
 	help
 	  Choose this option if you wish to use ion on Hisilicon Platform.
 
+config ION_TI
+	tristate "Ion for Texas Instruments"
+	depends on ION
+	help
+	  Choose this option if you wish to use ion on Texas Instruments platforms.
+
 source "drivers/staging/android/ion/hisilicon/Kconfig"
 
 config ION_OF

drivers/staging/android/ion/Makefile
@@ -11,5 +11,6 @@ endif
 obj-$(CONFIG_ION_DUMMY) += ion_dummy_driver.o
 obj-$(CONFIG_ION_TEGRA) += tegra/
 obj-$(CONFIG_ION_HISI) += hisilicon/
+obj-$(CONFIG_ION_TI) += ti/
 obj-$(CONFIG_ION_OF) += ion_of.o
 

drivers/staging/android/ion/ti/Makefile
@@ -1,0 +1,1 @@
+obj-$(CONFIG_ION_TI) += ti_ion.o

commit 1614a0de8d838d63490a9220cfdebccf27c8b066

staging: android: ion: Include sizes.h to get SZ_4k
On some platforms sizes.h is not included by default causing build
errors on these platforms, add this include here.

Fixes: dcbb0ca2550f ("TEMP: staging: android: ion: Add ion driver for TI platforms")
Reported-by: kbuild test robot <fengguang.wu@intel.com>
Acked-by: Tero Kristo <kristo@ti.com>
Signed-off-by: Andrew F. Davis <afd@ti.com>
Signed-off-by: Dan Murphy <dmurphy@ti.com>

drivers/staging/android/ion/ti/ti_ion.c
@@ -16,6 +16,7 @@
 
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/sizes.h>
 #include "../ion_priv.h"
 
 static struct ion_device *idev;