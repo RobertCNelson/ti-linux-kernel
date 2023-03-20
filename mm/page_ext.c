c46da3891371417e29d15a169cb966c44899cb98
Jaskaran Singh <quic_jasksing@quicinc.com>
2023-03-20 15:20:22 +0530

    ANDROID: mm: Export page_ext_[get|put]
    Export page_ext_get and page_ext_put for minidump.
    
    Bug: 394900912
    Bug: 199236943
    Change-Id: I63a9562ef2ea0ee4c2e7d6497b1424ceba9267eb
    Signed-off-by: Jaskaran Singh <quic_jasksing@quicinc.com>

--- mm/page_ext.c
+++ mm/page_ext.c
@@ -535,6 +535,7 @@ struct page_ext *page_ext_get(const struct page *page)
 
 	return page_ext;
 }
+EXPORT_SYMBOL_NS_GPL(page_ext_get, MINIDUMP);
 
 /**
  * page_ext_put() - Working with page extended information is done.
@@ -553,3 +554,4 @@ void page_ext_put(struct page_ext *page_ext)
 
 	rcu_read_unlock();
 }
+EXPORT_SYMBOL_NS_GPL(page_ext_put, MINIDUMP);
