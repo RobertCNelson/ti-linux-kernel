2cb178cea5eb5b0505a1e61b2f747fd478e4acc9
Mostafa Saleh <smostafa@google.com>
2023-08-02 11:19:08 +0000

    ANDROID: KVM: arm64: devices: Add initial device definitions
    In preparation for supporting device assignment in pKVM, the
    hypervisor should be able to authenticate assigned devices by
    providing HVCs to the guest to get trusted description about the
    device.
    
    To be able to do this the hypervisor should get assignable devices
    description at boot when the kernel is still trusted.
    
    Bug: 357781595
    Bug: 348382247
    Change-Id: I8bf38c253786eb56cbb1368531b2a3dea048ae88
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

---
+++ arch/arm64/kvm/hyp/nvhe/device/device.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2023 Google LLC
+ * Author: Mostafa Saleh <smostafa@google.com>
+ */
+
+struct pkvm_device *registered_devices;
+unsigned long registered_devices_nr;
