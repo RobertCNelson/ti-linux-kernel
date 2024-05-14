e8da2c8c483c7ef75c764479ee0e26f59493ea46
Jianan Huang <huangjianan@xiaomi.com>
2024-05-14 10:31:47 +0800

    ANDROID: Export cgroup function to allow module to remove control files
    Export cgroup_rm_cftypes to allow module to remove cgroup control files
    when exit, otherwise undefined behavior may occur.
    
    Bug: 340297716
    Change-Id: Ieda8a8ab155aeb71e0f20fdfb5068ac24465061f
    Signed-off-by: Jianan Huang <huangjianan@xiaomi.com>
    (cherry picked from commit 800f7297b5d0b17f00ad09e345513c4ba30d77d2)

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -4467,6 +4467,7 @@ int cgroup_rm_cftypes(struct cftype *cfts)
 	cgroup_unlock();
 	return 0;
 }
+EXPORT_SYMBOL_GPL(cgroup_rm_cftypes);
 
 /**
  * cgroup_add_cftypes - add an array of cftypes to a subsystem
