99ed6c1ace61e87e43c226237709accb5e03f25d
Bjorn Helgaas <bhelgaas@google.com>
2024-11-15 15:44:27 -0600

    UPSTREAM: PCI/pwrctrl: Rename pwrctl files to pwrctrl
    To slightly reduce confusion between "pwrctl" (the power controller and
    power sequencing framework) and "bwctrl" (the bandwidth controller),
    rename "pwrctl" to "pwrctrl" so they use the same "ctrl" suffix.
    
    Rename drivers/pci/pwrctl/ to drivers/pci/pwrctrl/, including the related
    MAINTAINERS, include file (include/linux/pci-pwrctl.h), Makefile, and
    Kconfig changes.
    
    This is the minimal rename of files only.  A subsequent commit will rename
    functions and data structures.
    
    Link: https://lore.kernel.org/r/20241115214428.2061153-2-helgaas@kernel.org
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
    Acked-by: Krzysztof Wilczyński <kw@linux.com>
    (cherry picked from commit b88cbaaa6fa109c2eb455d8fe2a318de0d197ea2)
    
    Bug: 380139395
    
    Change-Id: Ic62bba7be5bf652b995dd42bba9893a97c94d2a1
    Signed-off-by: Brian Norris <briannorris@google.com>

--- drivers/pci/pwrctrl/pci-pwrctrl-pwrseq.c
+++ drivers/pci/pwrctrl/pci-pwrctrl-pwrseq.c
@@ -6,7 +6,7 @@
 #include <linux/device.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
-#include <linux/pci-pwrctl.h>
+#include <linux/pci-pwrctrl.h>
 #include <linux/platform_device.h>
 #include <linux/property.h>
 #include <linux/pwrseq/consumer.h>
