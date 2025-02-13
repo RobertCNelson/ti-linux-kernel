3e74468f1e48840db8d1594e72fba33206d790c1
Suren Baghdasaryan <surenb@google.com>
2025-02-13 14:46:54 -0800

    FROMGIT: mm: make vma cache SLAB_TYPESAFE_BY_RCU
    To enable SLAB_TYPESAFE_BY_RCU for vma cache we need to ensure that
    object reuse before RCU grace period is over will be detected by
    lock_vma_under_rcu().
    
    Current checks are sufficient as long as vma is detached before it is
    freed.  The only place this is not currently happening is in exit_mmap().
    Add the missing vma_mark_detached() in exit_mmap().
    
    Another issue which might trick lock_vma_under_rcu() during vma reuse is
    vm_area_dup(), which copies the entire content of the vma into a new one,
    overriding new vma's vm_refcnt and temporarily making it appear as
    attached.  This might trick a racing lock_vma_under_rcu() to operate on a
    reused vma if it found the vma before it got reused.  To prevent this
    situation, we should ensure that vm_refcnt stays at detached state (0)
    when it is copied and advances to attached state only after it is added
    into the vma tree.  Introduce vm_area_init_from() which preserves new
    vma's vm_refcnt and use it in vm_area_dup().  Since all vmas are in
    detached state with no current readers when they are freed,
    
    lock_vma_under_rcu() will not be able to take vm_refcnt after vma got
    detached even if vma is reused. vma_mark_attached() in modified to
    include a release fence to ensure all stores to the vma happen before
    vm_refcnt gets initialized.
    
    Finally, make vm_area_cachep SLAB_TYPESAFE_BY_RCU. This will facilitate
    vm_area_struct reuse and will minimize the number of call_rcu() calls.
    
    Link: https://lkml.kernel.org/r/20250213224655.1680278-18-surenb@google.com
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Reviewed-by: Vlastimil Babka <vbabka@suse.cz>
    Tested-by: Shivank Garg <shivankg@amd.com>
      Link: https://lkml.kernel.org/r/5e19ec93-8307-47c2-bb13-3ddf7150624e@amd.com
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: David Howells <dhowells@redhat.com>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Hillf Danton <hdanton@sina.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Klara Modin <klarasmodin@gmail.com>
    Cc: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Cc: Lokesh Gidra <lokeshgidra@google.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Mateusz Guzik <mjguzik@gmail.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Mel Gorman <mgorman@techsingularity.net>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan@google.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: "Paul E . McKenney" <paulmck@kernel.org>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Sourav Panda <souravpanda@google.com>
    Cc: Wei Yang <richard.weiyang@gmail.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Heiko Carstens <hca@linux.ibm.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    (cherry picked from commit f56ae9bc0002a2ff7bf3cdd27ed847fe6e9d686a
    https: //git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git mm-unstable)
    Bug: 322132947
    Change-Id: I410c6fbce2e0d87ed5f7c19dc1f8806b2556837a
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- tools/testing/vma/linux/atomic.h
+++ tools/testing/vma/linux/atomic.h
@@ -7,6 +7,7 @@
 #define atomic_inc(x) uatomic_inc(x)
 #define atomic_read(x) uatomic_read(x)
 #define atomic_set(x, y) uatomic_set(x, y)
+#define atomic_set_release(x, y) uatomic_set(x, y)
 #define U8_MAX UCHAR_MAX
 
 #ifndef atomic_cmpxchg_relaxed
