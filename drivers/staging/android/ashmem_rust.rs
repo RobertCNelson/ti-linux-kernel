BayLibre/android_kernel_common

ANDROID: ashmem_rust: add show_fdinfo and GET_FILE_ID
These utilities let you get debug information about an ashmem file.

Change-Id: I3cbd9852a5e49bf0548927328eef171590c8ea96
Signed-off-by: Alice Ryhl <aliceryhl@google.com>

drivers/staging/android/ashmem_rust.rs
@@ -18,6 +18,8 @@ use kernel::{
     mm::virt::{flags as vma_flags, VmArea},
     page::page_align,
     prelude::*,
+    seq_file::SeqFile,
+    seq_print,
     sync::{new_mutex, Mutex},
     uaccess::{UserSlice, UserSliceReader, UserSliceWriter},
 };
@@ -213,6 +215,7 @@ impl MiscDevice for Ashmem {
             bindings::ASHMEM_GET_SIZE => me.get_size(),
             bindings::ASHMEM_SET_PROT_MASK => me.set_prot_mask(arg),
             bindings::ASHMEM_GET_PROT_MASK => me.get_prot_mask(),
+            bindings::ASHMEM_GET_FILE_ID => me.get_file_id(UserSlice::new(arg, size).writer()),
             _ => Err(EINVAL),
         }
     }
@@ -225,6 +228,20 @@ impl MiscDevice for Ashmem {
             _ => compat_cmd,
         };
         Self::ioctl(me, file, cmd, arg)
+    }
+
+    #[cfg(CONFIG_PROC_FS)]
+    fn show_fdinfo(me: Pin<&Ashmem>, _file: &File, m: &mut SeqFile) {
+        let asma = me.inner.lock();
+
+        if let Some(file) = asma.file.as_ref() {
+            seq_print!(m, "inode:\t{}", file.inode_ino());
+        }
+        if let Some(name) = asma.name.as_ref() {
+            let name = core::str::from_utf8(name).unwrap_or("<invalid utf-8>");
+            seq_print!(m, "name:\t{}", name);
+        }
+        seq_print!(m, "size\t{}", asma.size);
     }
 }
 
@@ -290,6 +307,17 @@ impl Ashmem {
 
     fn get_prot_mask(&self) -> Result<c_long> {
         Ok(self.inner.lock().prot_mask as c_long)
+    }
+
+    fn get_file_id(&self, mut writer: UserSliceWriter) -> Result<c_long> {
+        let asma = self.inner.lock();
+        let Some(file) = asma.file.as_ref() else {
+            return Err(EINVAL);
+        };
+        let ino = file.inode_ino();
+        drop(asma);
+        writer.write(&ino)?;
+        Ok(0)
     }
 }
 

RobertCNelson/ti-linux-kernel

Bug: 370906207
Change-Id: I5b9d00650272b3b814eb9e37a3b38123331f7624

drivers/staging/android/ashmem_rust.rs
@@ -20,6 +20,7 @@ use kernel::{
     mm::virt::{flags as vma_flags, VmAreaNew},
     page::page_align,
     prelude::*,
+    seq_file::{seq_print, SeqFile},
     sync::{new_mutex, Mutex},
     task::Task,
     uaccess::{UserSlice, UserSliceReader, UserSliceWriter},
@@ -208,6 +209,7 @@ impl MiscDevice for Ashmem {
             bindings::ASHMEM_GET_SIZE => me.get_size(),
             bindings::ASHMEM_SET_PROT_MASK => me.set_prot_mask(arg),
             bindings::ASHMEM_GET_PROT_MASK => me.get_prot_mask(),
+            bindings::ASHMEM_GET_FILE_ID => me.get_file_id(UserSlice::new(arg, size).writer()),
             _ => Err(EINVAL),
         }
     }
@@ -220,6 +222,19 @@ impl MiscDevice for Ashmem {
             _ => compat_cmd,
         };
         Self::ioctl(me, file, cmd, arg)
+    }
+
+    fn show_fdinfo(me: Pin<&Ashmem>, m: &SeqFile, _file: &File) {
+        let asma = me.inner.lock();
+
+        if let Some(file) = asma.file.as_ref() {
+            seq_print!(m, "inode:\t{}\n", file.inode_ino());
+        }
+        if let Some(name) = asma.name.as_ref() {
+            let name = core::str::from_utf8(name).unwrap_or("<invalid utf-8>");
+            seq_print!(m, "name:\t{}\n", name);
+        }
+        seq_print!(m, "size\t{}\n", asma.size);
     }
 }
 
@@ -297,6 +312,18 @@ impl Ashmem {
 
     fn get_prot_mask(&self) -> Result<isize> {
         Ok(self.inner.lock().prot_mask as isize)
+    }
+
+    fn get_file_id(&self, mut writer: UserSliceWriter) -> Result<isize> {
+        let ino = {
+            let asma = self.inner.lock();
+            let Some(file) = asma.file.as_ref() else {
+                return Err(EINVAL);
+            };
+            file.inode_ino()
+        };
+        writer.write(&ino)?;
+        Ok(0)
     }
 }
 