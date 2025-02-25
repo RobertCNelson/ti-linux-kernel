aa91035a5f252e57d344fb1f496aa0e0fa5b1c47
Ming Lei <ming.lei@redhat.com>
2025-02-25 10:21:41 +0800

    BACKPORT: block: make segment size limit workable for > 4K PAGE_SIZE
    Using PAGE_SIZE as a minimum expected DMA segment size in consideration
    of devices which have a max DMA segment size of < 64k when used on 64k
    PAGE_SIZE systems leads to devices not being able to probe such as
    eMMC and Exynos UFS controller [0] [1] you can end up with a probe failure
    as follows:
    
    WARNING: CPU: 2 PID: 397 at block/blk-settings.c:339 blk_validate_limits+0x364/0x3c0
    
    Ensure we use min(max_seg_size, seg_boundary_mask + 1) as the new min segment
    size when max segment size is < PAGE_SIZE for 16k and 64k base page size systems.
    
    If anyone need to backport this patch, the following commits are depended:
    
    	commit 6aeb4f836480 ("block: remove bio_add_pc_page")
    	commit 02ee5d69e3ba ("block: remove blk_rq_bio_prep")
    	commit b7175e24d6ac ("block: add a dma mapping iterator")
    
    Bug: 399192075
    
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>
    Link: https://lore.kernel.org/linux-block/20230612203314.17820-1-bvanassche@acm.org/ # [0]
    Link: https://lore.kernel.org/linux-block/1d55e942-5150-de4c-3a02-c3d066f87028@acm.org/ # [1]
    Cc: Yi Zhang <yi.zhang@redhat.com>
    Cc: John Garry <john.g.garry@oracle.com>
    Cc: Keith Busch <kbusch@kernel.org>
    Tested-by: Paul Bunyan <pbunyan@redhat.com>
    Reviewed-by: Daniel Gomez <da.gomez@kernel.org>
    Reviewed-by: Luis Chamberlain <mcgrof@kernel.org>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Ming Lei <ming.lei@redhat.com>
    Link: https://lore.kernel.org/r/20250225022141.2154581-1-ming.lei@redhat.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    (cherry picked from commit 889c57066ceee5e9172232da0608a8ac053bb6e5)
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>
    
    [dhavale: resolved minor conflict in block/blk.h]
    
    Change-Id: I5fe54dd8c73621259cbd9720b77253d8a2af29c7

--- block/blk.h
+++ block/blk.h
@@ -13,6 +13,8 @@
 
 struct elevator_type;
 
+#define	BLK_MIN_SEGMENT_SIZE	4096
+
 /* Max future timer expiry for timeouts */
 #define BLK_MAX_TIMEOUT		(5 * HZ)
 
@@ -357,8 +359,12 @@ struct bio *bio_split_zone_append(struct bio *bio,
 static inline bool bio_may_need_split(struct bio *bio,
 		const struct queue_limits *lim)
 {
-	return lim->chunk_sectors || bio->bi_vcnt != 1 ||
-		bio->bi_io_vec->bv_len + bio->bi_io_vec->bv_offset > PAGE_SIZE;
+	if (lim->chunk_sectors)
+		return true;
+	if (bio->bi_vcnt != 1)
+		return true;
+	return bio->bi_io_vec->bv_len + bio->bi_io_vec->bv_offset >
+		lim->min_segment_size;
 }
 
 /**
