30b1b9a350b475078938effa07e190848080a0d0
Neill Kapron <nkapron@google.com>
2025-03-13 19:15:27 +0000

    Revert "FROMLIST: ALSA: usb-audio: Export USB SND APIs for modules"
    This reverts commit 0f3a199f3cafb7f591c3519ebccd62bebba2e8aa.
    
    Reason for revert: This is not built via GKI and therefore we do
    not have a way to support or test/validate this patchset. Please
    implement this functionality as a vendor module.
    
    Change-Id: I644ba3e4c40a4492e4dcc3d23312860c31553d0b
    Signed-off-by: Neill Kapron <nkapron@google.com>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -1030,7 +1030,6 @@ int snd_usb_lock_shutdown(struct snd_usb_audio *chip)
 		wake_up(&chip->shutdown_wait);
 	return err;
 }
-EXPORT_SYMBOL_GPL(snd_usb_lock_shutdown);
 
 /* autosuspend and unlock the shutdown */
 void snd_usb_unlock_shutdown(struct snd_usb_audio *chip)
@@ -1039,7 +1038,6 @@ void snd_usb_unlock_shutdown(struct snd_usb_audio *chip)
 	if (atomic_dec_and_test(&chip->usage_count))
 		wake_up(&chip->shutdown_wait);
 }
-EXPORT_SYMBOL_GPL(snd_usb_unlock_shutdown);
 
 int snd_usb_autoresume(struct snd_usb_audio *chip)
 {
@@ -1062,7 +1060,6 @@ int snd_usb_autoresume(struct snd_usb_audio *chip)
 	}
 	return 0;
 }
-EXPORT_SYMBOL_GPL(snd_usb_autoresume);
 
 void snd_usb_autosuspend(struct snd_usb_audio *chip)
 {
@@ -1076,7 +1073,6 @@ void snd_usb_autosuspend(struct snd_usb_audio *chip)
 	for (i = 0; i < chip->num_interfaces; i++)
 		usb_autopm_put_interface(chip->intf[i]);
 }
-EXPORT_SYMBOL_GPL(snd_usb_autosuspend);
 
 static int usb_audio_suspend(struct usb_interface *intf, pm_message_t message)
 {
