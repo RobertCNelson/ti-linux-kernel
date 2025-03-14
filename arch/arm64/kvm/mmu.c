BayLibre/android_kernel_common

ANDROID: KVM: arm64: Use host_donate_guest sglist version
Rely on the newly introduced __pkvm_host_donate_guest_sglist HVC to group
page-table updates within a single 'atomic' operation.

Bug: 357781595
Bug: 361026567
Change-Id: I83e2c7621cb3262ae3c350d0eff77eb7c13129f3
Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

arch/arm64/kvm/mmu.c
@@ -1804,9 +1804,84 @@ static int __pkvm_topup_stage2_memcache(struct kvm_vcpu *vcpu, struct list_head 
 	return topup_hyp_memcache_account(vcpu->kvm, hyp_memcache, nr_stage2_pages, 0);
 }
 
+static int __pkvm_host_donate_guest_sglist(struct kvm *kvm, struct list_head *ppages)
+{
+	struct kvm_hyp_pinned_page *hyp_ppages;
+	int ret;
+
+	hyp_ppages = (struct kvm_hyp_pinned_page *)get_zeroed_page(GFP_KERNEL_ACCOUNT);
+	if (!hyp_ppages)
+		return -ENOMEM;
+
+	do {
+		struct kvm_hyp_pinned_page *hyp_ppage = NULL;
+		struct kvm_pinned_page *tmp, *ppage;
+		int p, nr_ppages = 0;
+
+		hyp_ppage = NULL;
+
+		list_for_each_entry(ppage, ppages, list_node) {
+			u64 pfn = page_to_pfn(ppage->page);
+			gfn_t gfn = ppage->ipa >> PAGE_SHIFT;
+
+			hyp_ppage = next_kvm_hyp_pinned_page(hyp_ppages, hyp_ppage, false);
+			if (!hyp_ppage) {
+				ret = -ENOMEM;
+				goto end;
+			}
+
+			hyp_ppage->pfn = pfn;
+			hyp_ppage->gfn = gfn;
+			hyp_ppage->order = ppage->order;
+			nr_ppages++;
+
+			/* Limit the time spent at EL2 */
+			if (nr_ppages >= 32)
+				break;
+		}
+
+		hyp_ppage = next_kvm_hyp_pinned_page(hyp_ppages, hyp_ppage, false);
+		if (!hyp_ppage) {
+			ret = -ENOMEM;
+			goto end;
+		}
+
+		hyp_ppage->order = ~((u8)0);
+
+		ret = kvm_call_hyp_nvhe(__pkvm_host_donate_guest_sglist,
+					(unsigned long)hyp_ppages);
+		/* See __pkvm_host_donate_guest() -EPERM comment */
+		if (ret == -EPERM) {
+			ret = 0;
+			goto end;
+		} else if (ret) {
+			goto end;
+		}
+
+		p = 0;
+		write_lock(&kvm->mmu_lock);
+		list_for_each_entry_safe(ppage, tmp, ppages, list_node) {
+			if (p++ >= nr_ppages)
+				break;
+
+			list_del(&ppage->list_node);
+			ppage->node.rb_right = ppage->node.rb_left = NULL;
+			WARN_ON(insert_ppage(kvm, ppage));
+		}
+		write_unlock(&kvm->mmu_lock);
+	} while (!list_empty(ppages));
+
+end:
+	free_pages((unsigned long)hyp_ppages, 0);
+	return ret;
+}
+
 static int __pkvm_host_donate_guest(struct kvm *kvm, struct list_head *ppages)
 {
 	struct kvm_pinned_page *ppage, *tmp;
+
+	if (ppages->next != ppages->prev && kvm->arch.pkvm.enabled)
+		return __pkvm_host_donate_guest_sglist(kvm, ppages);
 
 	list_for_each_entry_safe(ppage, tmp, ppages, list_node) {
 		u64 pfn = page_to_pfn(ppage->page);

RobertCNelson/ti-linux-kernel

arch/arm64/kvm/mmu.c
@@ -1804,12 +1804,78 @@ static int __pkvm_topup_stage2_memcache(struct kvm_vcpu *vcpu, struct list_head 
 	return topup_hyp_memcache_account(vcpu->kvm, hyp_memcache, nr_stage2_pages, 0);
 }
 
-static int __pkvm_host_donate_guest(struct kvm *kvm, struct list_head *ppages)
+static int __pkvm_host_donate_guest_sglist(struct kvm_vcpu *vcpu, struct list_head *ppages)
+{
+	struct kvm *kvm = vcpu->kvm;
+	int ret;
+
+	lockdep_assert_held_write(&kvm->mmu_lock);
+
+	do {
+		struct kvm_hyp_pinned_page *hyp_ppage = NULL;
+		struct kvm_pinned_page *tmp, *ppage;
+		int p, nr_ppages = 0;
+
+		list_for_each_entry(ppage, ppages, list_node) {
+			u64 pfn = page_to_pfn(ppage->page);
+			gfn_t gfn = ppage->ipa >> PAGE_SHIFT;
+
+			hyp_ppage = next_kvm_hyp_pinned_page(vcpu->arch.hyp_reqs, hyp_ppage, false);
+			if (!hyp_ppage)
+				break;
+
+			hyp_ppage->pfn = pfn;
+			hyp_ppage->gfn = gfn;
+			hyp_ppage->order = ppage->order;
+			nr_ppages++;
+
+			/* Limit the time spent at EL2 */
+			if (nr_ppages >= 32)
+				break;
+		}
+
+		if (hyp_ppage) {
+			hyp_ppage = next_kvm_hyp_pinned_page(vcpu->arch.hyp_reqs, hyp_ppage, false);
+			if (hyp_ppage)
+				hyp_ppage->order = ~((u8)0);
+		}
+
+		ret = kvm_call_hyp_nvhe(__pkvm_host_donate_guest_sglist);
+		/* See __pkvm_host_donate_guest() -EPERM comment */
+		if (ret == -EPERM) {
+			ret = 0;
+			break;
+		} else if (ret) {
+			break;
+		}
+
+		p = 0;
+		list_for_each_entry_safe(ppage, tmp, ppages, list_node) {
+			if (p++ >= nr_ppages)
+				break;
+
+			list_del(&ppage->list_node);
+			ppage->node.rb_right = ppage->node.rb_left = NULL;
+			WARN_ON(insert_ppage(kvm, ppage));
+		}
+	} while (!list_empty(ppages));
+
+	return ret;
+}
+
+static int __pkvm_host_donate_guest(struct kvm_vcpu *vcpu, struct list_head *ppages)
 {
 	struct kvm_pinned_page *ppage, *tmp;
+	struct kvm *kvm = vcpu->kvm;
 	int ret = -EINVAL; /* Empty list */
 
 	write_lock(&kvm->mmu_lock);
+
+	if (ppages->next != ppages->prev && kvm->arch.pkvm.enabled) {
+		ret = __pkvm_host_donate_guest_sglist(vcpu, ppages);
+		goto unlock;
+	}
+
 	list_for_each_entry_safe(ppage, tmp, ppages, list_node) {
 		u64 pfn = page_to_pfn(ppage->page);
 		gfn_t gfn = ppage->ipa >> PAGE_SHIFT;
@@ -1836,6 +1902,8 @@ static int __pkvm_host_donate_guest(struct kvm *kvm, struct list_head *ppages)
 		WARN_ON(insert_ppage(kvm, ppage));
 
 	}
+
+unlock:
 	write_unlock(&kvm->mmu_lock);
 
 	return ret;
@@ -1886,7 +1954,7 @@ static int pkvm_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa, size_t s
 		goto free_ppages;
 	account_dec = true;
 
-	ret = __pkvm_host_donate_guest(kvm, &ppages);
+	ret = __pkvm_host_donate_guest(vcpu, &ppages);
 
 free_ppages:
 	/* Pages left in the list haven't been mapped */