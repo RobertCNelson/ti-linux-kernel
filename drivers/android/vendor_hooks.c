dc524f0ad6134cb6539ba7d15599696c84ea1edb
Qixia Yang <yangqixia2@oppo.com>
2025-03-24 17:17:44 +0800

    ANDROID: vendor_hooks: Add hooks for exiting control
    Add hooks at process exiting routines so that oems
    can control these procedures. One possible benifit is the peak of
    system load can be shaved and load can be more smooth when a large
    number of threads is killed once upon a time, while a sudden peak
    of system load can probably lead to user junk issues.
    
    Bug: 296493318
    Change-Id: Ied4cc968e45baa39f1b79e8c7dce75cf6b8db55d
    Signed-off-by: Qixia Yang <yangqixia2@oppo.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -422,3 +422,4 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_reclaim_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alloc_pages_failure_bypass);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_save_vmalloc_stack);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_stack_hash);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_exit_check);
