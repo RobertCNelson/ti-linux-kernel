ede56033b2328d3cf136290e98636d732b349e9c
Keir Fraser <keirf@google.com>
2025-04-08 10:09:16 +0000

    Revert "ANDROID: KVM: arm64: iommu: Allow to boot without IOMMU driver"
    This reverts commit 247011a0ccb858e0a179ddbbfea0ae755cc9fed5.
    
    We require an IOMMU for pVM confidentiality.
    
    Bug: 357781595
    Bug: 384432312
    Change-Id: I858ed7063ef3fc02cef1155c21b7ffdf921a333e
    Signed-off-by: Keir Fraser <keirf@google.com>

--- arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
+++ arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -215,7 +215,7 @@ int kvm_iommu_init(struct kvm_iommu_ops *ops,
 	    !ops->init ||
 	    !ops->alloc_domain ||
 	    !ops->free_domain)
-		return 0;
+		return -ENODEV;
 
 	ret = hyp_pool_init_empty(&iommu_host_pool, 64);
 	if (ret)
