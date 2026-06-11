beagleboard/mirror-ti-linux-kernel

commit 76fab2f299ecc417c5dcb296e029f7465ec74af0

PENDING: remoteproc: k3: Ensure mailbox is suspended after remoteproc
The remoteproc driver uses mailbox to communicate suspend and shutdown
messages to the remote core. Upon receiving this special message from
Linux, the remote core disables its mailbox RX interrupts by writing to
the corresponding MAILBOX_IRQSTATUS_CLR_u registers.

Therefore, we have to ensure mailbox driver is suspended after this
shutdown ack has been received from remote core (mbox restores its irq
registers in resume). Failing to do so, would result in restoring
incorrect irq configuration in resume leading to spurious mbox
interrupts to the remote core.

Therefore, use device_links to add mailbox device as a supplier to the
remoteproc device (consumer). This ensures that mailbox is suspended
after remoteproc. Further, register the mbox pm ops in the late_suspend
stage to match with that of remoteproc's stage.

Before:
omap-mailbox 31f82000.mailbox: PM: calling omap_mbox_suspend
   [omap_mailbox] @ 1168, parent: bus@100000:bus@30000000
k3_r5_rproc bus@100000:r5fss@5c00000: PM: calling k3_r5_suspend_late
   [ti_k3_r5_remoteproc] @ 1168, parent: bus@100000

After:
k3_r5_rproc bus@100000:r5fss@5e00000: PM: k3_r5_suspend_late
   [ti_k3_r5_remoteproc] returned 0 after 29077
omap-mailbox 31f85000.mailbox: PM: calling omap_mbox_suspend
   [omap_mailbox] @ 1138, parent:

Signed-off-by: Beleswar Padhi <b-padhi@ti.com>

drivers/remoteproc/ti_k3_r5_remoteproc.c
@@ -1917,6 +1917,9 @@ static int k3_r5_cluster_of_init(struct platform_device *pdev)
 	struct k3_r5_cluster *cluster = platform_get_drvdata(pdev);
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev_of_node(dev);
+	struct device_node *child_np;
+	struct device_node *mbox_np;
+	struct platform_device *mbox_pdev;
 	struct platform_device *cpdev;
 	struct device_node *child;
 	struct k3_r5_core *core;
@@ -1930,6 +1933,27 @@ static int k3_r5_cluster_of_init(struct platform_device *pdev)
 			of_node_put(child);
 			goto fail;
 		}
+
+		child_np = dev_of_node(&cpdev->dev);
+
+		mbox_np = of_parse_phandle(child_np, "mboxes", 0);
+		if (!mbox_np) {
+			dev_err(dev, "failed to get mboxes\n");
+			ret = -ENODEV;
+			goto fail;
+		}
+
+		mbox_pdev = of_find_device_by_node(mbox_np);
+		of_node_put(mbox_np);
+		if (!mbox_pdev) {
+			dev_err(dev, "mailbox device not yet ready\n");
+			ret = -EPROBE_DEFER;
+			goto fail;
+		}
+
+		/* Ensure mailbox is suspended after remoteproc */
+		device_link_add(dev, &mbox_pdev->dev,
+				DL_FLAG_AUTOREMOVE_SUPPLIER);
 
 		ret = k3_r5_core_of_init(cpdev);
 		if (ret) {

drivers/mailbox/omap-mailbox.c
@@ -379,7 +379,7 @@ static int omap_mbox_resume(struct device *dev)
 #endif
 
 static const struct dev_pm_ops omap_mbox_pm_ops = {
-	SET_SYSTEM_SLEEP_PM_OPS(omap_mbox_suspend, omap_mbox_resume)
+	LATE_SYSTEM_SLEEP_PM_OPS(omap_mbox_suspend, omap_mbox_resume)
 };
 
 static const struct omap_mbox_match_data omap2_data = { MBOX_INTR_CFG_TYPE1, true };

drivers/remoteproc/ti_k3_common.c
@@ -20,6 +20,7 @@
 #include <linux/module.h>
 #include <linux/of_address.h>
 #include <linux/of_device.h>
+#include <linux/of_platform.h>
 #include <linux/of_reserved_mem.h>
 #include <linux/omap-mailbox.h>
 #include <linux/platform_device.h>
@@ -230,6 +231,9 @@ int k3_rproc_request_mbox(struct rproc *rproc)
 	struct k3_rproc *kproc = rproc->priv;
 	struct mbox_client *client = &kproc->client;
 	struct device *dev = kproc->dev;
+	struct platform_device *mbox_pdev;
+	struct device_node *np = dev_of_node(dev);
+	struct device_node *mbox_np;
 
 	client->dev = dev;
 	client->tx_done = NULL;
@@ -241,6 +245,22 @@ int k3_rproc_request_mbox(struct rproc *rproc)
 	if (IS_ERR(kproc->mbox))
 		return dev_err_probe(dev, PTR_ERR(kproc->mbox),
 				     "mbox_request_channel failed\n");
+
+	mbox_np = of_parse_phandle(np, "mboxes", 0);
+	if (!mbox_np) {
+		dev_err(dev, "failed to get mboxes\n");
+		return -ENODEV;
+	}
+
+	mbox_pdev = of_find_device_by_node(mbox_np);
+	of_node_put(mbox_np);
+	if(!mbox_pdev) {
+		dev_err(dev, "mailbox device not yet ready\n");
+		return -EPROBE_DEFER;
+	}
+
+	/* Ensure mailbox is suspended after remoteproc */
+	device_link_add(dev, &mbox_pdev->dev, DL_FLAG_AUTOREMOVE_SUPPLIER);
 
 	return 0;
 }

RobertCNelson/ti-linux-kernel

commit 0c2a839ac030ec3222d602f223a36cbeec224ae9

PENDING: remoteproc: k3-r5: Move device link setup to request_mbox
Remoteproc can probe before mailbox. The k3_r5_rproc_request_mbox()
function already handles deferred probe if mailbox is not ready.
Therefore, Move device_link_add() from k3_r5_cluster_of_init() to
k3_r5_rproc_request_mbox() to ensure the device link is added only after
mailbox is probed and available.

Fixes: 76fab2f299ec ("PENDING: remoteproc: k3: Ensure mailbox is suspended after remoteproc")
Signed-off-by: Beleswar Padhi <b-padhi@ti.com>
Reviewed-by: Andrew Davis <afd@ti.com>
Tested-by: Abhash Kumar Jha <a-kumar2@ti.com>

drivers/remoteproc/ti_k3_r5_remoteproc.c
@@ -495,6 +495,10 @@ static int k3_r5_rproc_request_mbox(struct rproc *rproc)
 	struct k3_r5_rproc *kproc = rproc->priv;
 	struct mbox_client *client = &kproc->client;
 	struct device *dev = kproc->dev;
+	struct platform_device *mbox_pdev;
+	struct device_node *np = dev_of_node(dev);
+	struct device_node *mbox_np;
+	struct device_link *link;
 
 	client->dev = dev;
 	client->tx_done = NULL;
@@ -506,6 +510,22 @@ static int k3_r5_rproc_request_mbox(struct rproc *rproc)
 	if (IS_ERR(kproc->mbox))
 		return dev_err_probe(dev, PTR_ERR(kproc->mbox),
 				     "mbox_request_channel failed\n");
+
+	mbox_np = of_parse_phandle(np, "mboxes", 0);
+	if (IS_ERR(mbox_np))
+		return dev_err_probe(dev, PTR_ERR(mbox_np), "failed to get mboxes\n");
+
+	mbox_pdev = of_find_device_by_node(mbox_np);
+	of_node_put(mbox_np);
+	if (IS_ERR(mbox_pdev))
+		return dev_err_probe(dev, PTR_ERR(mbox_pdev), "mailbox device not yet ready\n");
+
+	/* Ensure mailbox is suspended after remoteproc */
+	link = device_link_add(dev, &mbox_pdev->dev, DL_FLAG_AUTOREMOVE_SUPPLIER);
+	put_device(&mbox_pdev->dev);
+	if (IS_ERR(link))
+		return dev_err_probe(dev, PTR_ERR(link),
+				     "Unable to create device link with mbox dev\n");
 
 	return 0;
 }
@@ -1927,10 +1947,6 @@ static int k3_r5_cluster_of_init(struct platform_device *pdev)
 	struct k3_r5_cluster *cluster = platform_get_drvdata(pdev);
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev_of_node(dev);
-	struct device_node *child_np;
-	struct device_node *mbox_np;
-	struct platform_device *mbox_pdev;
-	struct device_link *link;
 	struct platform_device *cpdev;
 	struct device_node *child;
 	struct k3_r5_core *core;
@@ -1944,31 +1960,6 @@ static int k3_r5_cluster_of_init(struct platform_device *pdev)
 			of_node_put(child);
 			goto fail;
 		}
-
-		child_np = dev_of_node(&cpdev->dev);
-
-		mbox_np = of_parse_phandle(child_np, "mboxes", 0);
-		if (!mbox_np) {
-			dev_err(dev, "failed to get mboxes\n");
-			ret = -ENODEV;
-			goto fail;
-		}
-
-		mbox_pdev = of_find_device_by_node(mbox_np);
-		of_node_put(mbox_np);
-		if (!mbox_pdev) {
-			dev_err(dev, "mailbox device not yet ready\n");
-			ret = -EPROBE_DEFER;
-			goto fail;
-		}
-
-		/* Ensure mailbox is suspended after remoteproc */
-		link = device_link_add(dev, &mbox_pdev->dev,
-				       DL_FLAG_AUTOREMOVE_SUPPLIER);
-		put_device(&mbox_pdev->dev);
-		if (IS_ERR(link))
-			return dev_err_probe(dev, PTR_ERR(link),
-					     "Unable to create device link with mbox dev\n");
 
 		ret = k3_r5_core_of_init(cpdev);
 		if (ret) {