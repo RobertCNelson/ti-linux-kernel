435b1d0e4c5f41170a5d0dff48a9f6d739872bfd
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2026-07-01 17:36:53 +0530

    FROMLIST: hsr: Add a magic header for sending PTP packets
    Sending PTP packets (ETH_P_1588) via the HSR stack is pointless in its
    current shape because the requested PTP timestamp is not routed to the
    sender. It also needs to be distinguished on which port the message
    should be sent and whether or not a HSR should be attached.
    
    To pass this information, a custom header (struct hsr_inline_header) is
    introduced. This header is expected if the ether type is ETH_P_1588. To
    avoid any wrong usage, there is a magic field to ensure it really is the
    header.
    
    The inline header is just prepended containing the port and header
    parameter. After the header, a regular packet follows. It is ensured that
    the inline header and the following ethernet of hsr header is linear
    and can be accessed.
    
    The two retrieved parameters are passed to hsr_forward_skb() and will be
    used later.
    
    Link: https://lore.kernel.org/all/20260527-hsr_ptp-v5-3-158a7633eac0@linutronix.de/
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: MD Danish Anwar <danishanwar@ti.com>

--- net/hsr/hsr_device.c
+++ net/hsr/hsr_device.c
@@ -364,24 +364,59 @@ static netdev_features_t hsr_fix_features(struct net_device *dev,
 
 static netdev_tx_t hsr_dev_xmit(struct sk_buff *skb, struct net_device *dev)
 {
+	enum hsr_port_type tx_port = HSR_PT_NONE;
 	struct hsr_priv *hsr = netdev_priv(dev);
 	struct hsr_port *master;
+	bool has_header = false;
 
 	rcu_read_lock();
 	master = hsr_port_get_hsr(hsr, HSR_PT_MASTER);
-	if (master) {
-		skb->dev = master->dev;
-		skb_reset_mac_header(skb);
-		skb_reset_mac_len(skb);
-		spin_lock_bh(&hsr->seqnr_lock);
-		hsr_forward_skb(skb, master);
-		spin_unlock_bh(&hsr->seqnr_lock);
-	} else {
-		dev_core_stats_tx_dropped_inc(dev);
-		dev_kfree_skb_any(skb);
+	if (!master)
+		goto drop;
+
+	skb->dev = master->dev;
+	if (skb->protocol == htons(ETH_P_1588)) {
+		struct hsr_inline_header *hsr_opt;
+		unsigned int hdr_len;
+
+		BUILD_BUG_ON(sizeof(struct hsr_inline_header) != sizeof(struct ethhdr));
+
+		/* need to access the magic header header */
+		if (!pskb_may_pull(skb, sizeof(struct hsr_inline_header)))
+			goto drop;
+
+		hsr_opt = (struct hsr_inline_header *)skb_mac_header(skb);
+		if (hsr_opt->magic == htonl(HSR_INLINE_HDR)) {
+			struct ethhdr *eth_hdr;
+
+			has_header = hsr_opt->hsr_hdr;
+			tx_port = hsr_opt->tx_port;
+			if (tx_port != HSR_PT_SLAVE_A && tx_port != HSR_PT_SLAVE_B)
+				goto drop;
+
+			eth_hdr = skb_pull(skb, sizeof(struct hsr_inline_header));
+			if (has_header)
+				hdr_len = ETH_HLEN + HSR_HLEN;
+			else
+				hdr_len = ETH_HLEN;
+			skb_set_network_header(skb, hdr_len);
+			/* Ensure the header can be accessed */
+			if (!pskb_may_pull(skb, hdr_len))
+				goto drop;
+			skb->protocol = eth_hdr->h_proto;
+		}
 	}
+	skb_reset_mac_header(skb);
+	skb_reset_mac_len(skb);
+	spin_lock_bh(&hsr->seqnr_lock);
+	hsr_forward_skb(skb, master, tx_port, has_header);
+	spin_unlock_bh(&hsr->seqnr_lock);
 	rcu_read_unlock();
 
+	return NETDEV_TX_OK;
+drop:
+	dev_core_stats_tx_dropped_inc(dev);
+	dev_kfree_skb_any(skb);
 	return NETDEV_TX_OK;
 }
 
@@ -503,7 +538,7 @@ static void send_hsr_supervision_frame(struct hsr_port *port,
 		return;
 	}
 
-	hsr_forward_skb(skb, port);
+	hsr_forward_skb(skb, port, HSR_PT_NONE, false);
 	spin_unlock_bh(&hsr->seqnr_lock);
 	return;
 }
@@ -544,7 +579,7 @@ static void send_prp_supervision_frame(struct hsr_port *master,
 		return;
 	}
 
-	hsr_forward_skb(skb, master);
+	hsr_forward_skb(skb, master, HSR_PT_NONE, false);
 	spin_unlock_bh(&hsr->seqnr_lock);
 }
 
